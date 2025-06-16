246902a7eb9cf4997615e89541a42a15c9319919
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-06-15 23:01:27 -0700

    LC-majority-element [Runtime: N/A; Memory: N/A]

--- array/majority-element.py
+++ array/majority-element.py
@@ -1,4 +1,6 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        if len(nums) == 0:
+            return [] 
         nums.sort()
         return nums[len(nums) // 2 + 1]
