84be6817fd72295a30821ce6e5353f6edc5d7043
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-06-18 21:04:23 -0700

    LC-rotate-array [Runtime: N/A; Memory: N/A]

---
+++ math/rotate-array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        nums.reverse()
+        nums[:k] = reversed(nums[:k])
+        nums[k:] = reversed(nums[k:])
