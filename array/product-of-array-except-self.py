0db79d7c6f47fd68556754ff7e06c47a1c360bd8
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-06-28 22:48:42 -0700

    LC-product-of-array-except-self [Runtime: 24 ms; Memory: 23.4 MB]

--- array/product-of-array-except-self.py
+++ array/product-of-array-except-self.py
@@ -1,13 +1,14 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         n = len(nums)
-        left = [1] * n
-        right = [1] * n
         output = [1] * n
-        for i in range(1, n):
-            left[i] = left[i-1] * nums[i-1]
-        for i in range(n-2, -1, -1):
-            right[i] = right[i+1] * nums[i+1]
+
+        left = 1
         for i in range(n):
-            output[i] = left[i] * right[i]
+            output[i] *= left
+            left *= nums[i]
+        right = 1
+        for i in range(n - 1, -1, -1):
+            output[i] *= right
+            right *= nums[i]
         return output
