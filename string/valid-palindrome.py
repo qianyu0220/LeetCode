4884c15b8d42c61f3aeb38caac31ac3d69f3d6c1
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-07-02 14:22:14 -0700

    LC-valid-palindrome [Runtime: N/A; Memory: N/A]

---
+++ string/valid-palindrome.py
@@ -0,0 +1,7 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        tmp = ""
+        for char in s.lower():
+            if char.isdigit() or char.isalpha():
+                tmp += char
+        return tmp == tmp[:-1:-1]
