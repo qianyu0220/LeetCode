28b8b460d791763984fb019e7b15b7f51f53f1eb
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-07-04 13:36:49 -0700

    LC-jump-game [Runtime: 39 ms; Memory: 18.7 MB]

--- dynamic-programming/jump-game.py
+++ dynamic-programming/jump-game.py
@@ -4,5 +4,5 @@ class Solution:
         for i in range(len(nums)):
             if i > maxReach:
                 return False
-            maxReach = max(maxReach, i + nums[i])
+            maxReach = max(i + nums[i], maxReach)
         return True
