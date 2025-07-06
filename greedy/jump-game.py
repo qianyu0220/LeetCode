31ea157a54b65faa88502cae9901a3f0635c6d7a
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-07-05 22:26:38 -0700

    LC-jump-game [Runtime: N/A; Memory: N/A]

--- greedy/jump-game.py
+++ greedy/jump-game.py
@@ -2,7 +2,7 @@ class Solution:
     def canJump(self, nums: List[int]) -> bool:
         maxReach = 0
         for i in range(len(nums)):
-            if i > maxReach:
-                return False
             maxReach = max(maxReach, i + nums[i])
+            if i == maxReach:
+                return False
         return True
