347e191eff33d109643c3d94cb9af96b7133d00e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-07-05 22:52:49 -0700

    LC-product-of-array-except-self [Runtime: 23 ms; Memory: 23.2 MB]

--- prefix-sum/product-of-array-except-self.py
+++ prefix-sum/product-of-array-except-self.py
@@ -4,13 +4,11 @@ class Solution:
         output = [1] * n
 
         left = 1
+        right = 1
         for i in range(n):
             output[i] *= left
             left *= nums[i]
-                  
-        right = 1
         for i in range(n-1, -1, -1):
             output[i] *= right
             right *= nums[i]
-            
         return output
