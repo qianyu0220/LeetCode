9e850347f35b2e78cb20b5a7eb5cf67075aeabb4
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-07-05 22:48:25 -0700

    LC-product-of-array-except-self [Runtime: 24 ms; Memory: 23.3 MB]

--- array/product-of-array-except-self.py
+++ array/product-of-array-except-self.py
@@ -7,8 +7,10 @@ class Solution:
         for i in range(n):
             output[i] *= left
             left *= nums[i]
+                  
         right = 1
-        for i in range(n - 1, -1, -1):
+        for i in range(n-1, -1, -1):
             output[i] *= right
             right *= nums[i]
+            
         return output
