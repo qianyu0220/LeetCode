1457f509b786846a6c8349a00232d6bea9cde792
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-07-30 22:57:18 -0700

    LC-ransom-note [Runtime: 11 ms; Memory: 17.8 MB]

--- counting/ransom-note.py
+++ counting/ransom-note.py
@@ -1,13 +1,9 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        output = ""
-        p1 = 0
-        p2 = 0
-        while p1 < len(ransomNote) and p2 < len(magazine):
-            if ransomNote[p1] == magazine[p2]:
-                output += ransomNote[p1]
-                p1 += 1
-                p2 += 1
-            else:
-                p2 += 1
-        return output == ransomNote
+        r = collections.Counter(ransomNote)
+        m = collections.Counter(magazine)
+
+        if r & m == r:
+            return True
+        else:
+            return False
