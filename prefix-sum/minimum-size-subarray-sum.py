f407b6f53c4cb3460e19376cd05e437f0b9bbc73
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-14 19:43:23 -0700

    LC-minimum-size-subarray-sum [Runtime: 8 ms; Memory: 28.4 MB]

--- prefix-sum/minimum-size-subarray-sum.py
+++ prefix-sum/minimum-size-subarray-sum.py
@@ -1,13 +1,13 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        n = len(nums)
         ans = float("inf")
-        window_sum = 0
         left = 0
+        n = len(nums)
+        window_sum = 0
         for right in range(n):
             window_sum += nums[right]
             while window_sum >= target:
                 ans = min(ans, right - left + 1)
                 window_sum -= nums[left]
                 left += 1
-        return 0 if ans == float("inf") else ans
+        return 0 if ans==float("inf") else ans
