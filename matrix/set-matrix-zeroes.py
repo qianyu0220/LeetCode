6a97fe1cfd307ba46dd09840622d74149dbfa0cb
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-15 15:03:05 -0700

    LC-set-matrix-zeroes [Runtime: 3 ms; Memory: 18.6 MB]

---
+++ matrix/set-matrix-zeroes.py
@@ -0,0 +1,19 @@
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        rows, cols = set(), set()
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == 0:
+                    rows.add(i)
+                    cols.add(j)
+        for i in rows:
+            for j in range(n):
+                matrix[i][j] = 0
+        for i in cols:
+            for j in range(m):
+                matrix[j][i] = 0
