b71b4d6ed615554bb1b74fef3735aa193d48923f
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-16 11:16:32 -0700

    LC-isomorphic-strings [Runtime: N/A; Memory: N/A]

--- hash-table/isomorphic-strings.py
+++ hash-table/isomorphic-strings.py
@@ -5,13 +5,13 @@ class Solution:
         s_to_t, t_to_s = {}, {}
         for char_s, char_t in zip(s, t):
             if char_s in s_to_t:
-                if s_to_t[char_s] != char_t:
-                    return False
-            else:
-                s_to_t[char_s] = char_t
+                if s_to_t[char_s] == char_t:
+                    return True
+            s_to_t[char_s] = char_t
             if char_t in t_to_s:
-                if t_to_s[char_t] != char_s:
-                    return False
-            else:
-                t_to_s[char_t] = char_s
-        return True
+                if t_to_s[char_t] == char_s:
+                    return True
+            t_to_s[char_t] = char_s
+
+        return False
+                
