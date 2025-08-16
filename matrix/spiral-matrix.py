139c2cb5d3a1c54f5984f419542e150094ed95de
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-15 17:53:25 -0700

    LC-spiral-matrix [Runtime: 0 ms; Memory: 17.7 MB]

--- matrix/spiral-matrix.py
+++ matrix/spiral-matrix.py
@@ -1,31 +1,29 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        if not matrix or not matrix[0]:
-            return []
         m, n = len(matrix), len(matrix[0])
-        top, bottom = 0, m-1
-        left, right = 0, n-1
+        top, bottom, left, right = 0, m-1, 0, n-1
         ans = []
-        while top <= bottom and left <= right:
+        while left <= right and top <= bottom:
             for i in range(left, right+1):
                 ans.append(matrix[top][i])
             top += 1
             if top > bottom:
                 break
-            
+
             for i in range(top, bottom+1):
                 ans.append(matrix[i][right])
             right -= 1
             if left > right:
                 break
-            
+
             for i in range(right, left-1, -1):
                 ans.append(matrix[bottom][i])
             bottom -= 1
             if top > bottom:
                 break
-
+            
             for i in range(bottom, top-1, -1):
                 ans.append(matrix[i][left])
             left += 1
+
         return ans
