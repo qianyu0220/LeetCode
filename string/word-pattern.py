0a90ea8ea739932c53b837fae9e6bcaee9d0d790
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-16 11:22:25 -0700

    LC-word-pattern [Runtime: 0 ms; Memory: 17.9 MB]

--- string/word-pattern.py
+++ string/word-pattern.py
@@ -1,4 +1,4 @@
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
         s = s.split()
-        return len(set(s)) == len(set(pattern)) == len(set(zip_longest(s, pattern)))
+        return len(set(s)) == len(set(pattern)) == len(set(zip_longest(pattern, s)))
