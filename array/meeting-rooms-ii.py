e5882d8584be914868924f9797b6e817416a563b
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-18 22:58:08 -0700

    LC-meeting-rooms-ii [Runtime: 4 ms; Memory: 20.2 MB]

---
+++ array/meeting-rooms-ii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        if not intervals:
+            return 0
+        starts = sorted([s for s, _ in intervals])
+        ends = sorted([e for _, e in intervals])
+        i = j = 0
+        rooms = 0
+        max_rooms = 0
+        while i < len(starts):
+            if starts[i] < ends[j]:
+                rooms += 1
+                max_rooms = max(rooms, max_rooms)
+                i += 1
+            else:
+                rooms -= 1
+                j += 1
+        return max_rooms
