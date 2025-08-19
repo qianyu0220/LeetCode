aaecfbe54d4dcd8b4d08545f0b49a4e6c35ec2dc
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-19 14:17:59 -0700

    LC-meeting-rooms-ii [Runtime: 5 ms; Memory: 19.8 MB]

--- greedy/meeting-rooms-ii.py
+++ greedy/meeting-rooms-ii.py
@@ -1,19 +1,16 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        if not intervals:
-            return 0
-        starts = sorted([s for s, _ in intervals])
-        ends = sorted([e for _, e in intervals])
+        starts = sorted(s for s, _ in intervals)
+        ends = sorted(e for _,e in intervals)
         i = j = 0
-        rooms = max_room = 0
+        rooms = 0
+        max_rooms = 0
         while i < len(starts):
             if starts[i] < ends[j]:
                 rooms += 1
-                
+                max_rooms = max(rooms, max_rooms)
                 i += 1
             else:
                 rooms -= 1
                 j += 1
-            max_room = max(max_room, rooms)
-        return max_room
-            
+        return max_rooms
