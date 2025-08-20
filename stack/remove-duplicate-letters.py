eac268e29f517e13dce025493488fbc41695203b
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-19 20:29:17 -0700

    LC-remove-duplicate-letters [Runtime: 0 ms; Memory: 17.7 MB]

---
+++ stack/remove-duplicate-letters.py
@@ -0,0 +1,14 @@
+class Solution:
+    def removeDuplicateLetters(self, s: str) -> str:
+        last_index = {ch: i for i, ch in enumerate(s)}
+        stack = []
+        in_stack = set()
+        for i, ch in enumerate(s):
+            if ch in in_stack:
+                continue
+            while stack and ch < stack[-1] and last_index[stack[-1]] > i:
+                removed = stack.pop()
+                in_stack.remove(removed)
+            stack.append(ch)
+            in_stack.add(ch)
+        return "".join(stack)
