5fffe07f658a0a28a5d820b387e9ecadbde41867
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-19 19:50:29 -0700

    LC-course-schedule [Runtime: 2 ms; Memory: 19 MB]

---
+++ breadth-first-search/course-schedule.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+        
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        count = 0
+
+        while queue:
+            cur = queue.popleft()
+            count += 1
+
+            for nxt in graph[cur]:
+                indegree[nxt] -= 1
+                if indegree[nxt] == 0:
+                    queue.append(nxt)
+        return count == numCourses
