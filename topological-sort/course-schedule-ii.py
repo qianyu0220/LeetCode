b775776172935a6d689f9579dc499b211c149924
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-19 20:13:32 -0700

    LC-course-schedule-ii [Runtime: 4 ms; Memory: 18.9 MB]

---
+++ topological-sort/course-schedule-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        graph = [[] for _ in range(numCourses)]
+        indegree = [0] * numCourses
+        for a, b in prerequisites:
+            graph[b].append(a)
+            indegree[a] += 1
+        queue = deque([i for i in range(numCourses) if indegree[i] == 0])
+        result = []
+        while queue:
+            cur = queue.popleft()
+            result.append(cur)
+            for nxt in graph[cur]:
+                indegree[nxt] -= 1
+                if indegree[nxt] == 0:
+                    queue.append(nxt)
+        return result if len(result) == numCourses else []
