3c315e6ded3abe1512b23cdb1cfe7b5f08ff69ef
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-20 22:49:58 -0700

    LC-count-number-of-nice-subarrays [Runtime: 131 ms; Memory: 23 MB]

--- prefix-sum/count-number-of-nice-subarrays.py
+++ prefix-sum/count-number-of-nice-subarrays.py
@@ -1,16 +1,16 @@
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        return self.atmost(nums, k) - self.atmost(nums, k-1)
+        return self.atmost(nums, k) - self.atmost(nums, k - 1)
     def atmost(self, nums, k):
         left = 0
         output = 0
-        count = 0
+        count_odd = 0
         for right, num in enumerate(nums):
             if num % 2 == 1:
-                count += 1
-            while count > k:                   
+                count_odd += 1
+            while count_odd > k:
                 if nums[left] % 2 == 1:
-                    count -= 1
+                    count_odd -= 1
                 left += 1
             output += right - left + 1
         return output
