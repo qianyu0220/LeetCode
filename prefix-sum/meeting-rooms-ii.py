de7983822fbac78190918b9b75f0acff7a6d30df
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-22 14:16:40 -0700

    LC-meeting-rooms-ii [Runtime: 3 ms; Memory: 20.1 MB]

--- prefix-sum/meeting-rooms-ii.py
+++ prefix-sum/meeting-rooms-ii.py
@@ -1,16 +1,15 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        starts = sorted(s for s, _ in intervals)
-        ends = sorted(e for _,e in intervals)
+        starts = sorted([s for s, e in intervals])
+        ends = sorted([e for s, e in intervals])
         i = j = 0
-        rooms = 0
-        max_rooms = 0
+        rooms = max_room = 0
         while i < len(starts):
             if starts[i] < ends[j]:
                 rooms += 1
-                max_rooms = max(rooms, max_rooms)
+                max_room = max(rooms, max_room)
                 i += 1
             else:
                 rooms -= 1
                 j += 1
-        return max_rooms
+        return max_room
