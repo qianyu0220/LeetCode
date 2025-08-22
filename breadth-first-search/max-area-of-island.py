52bffeb3a7972499633b96a1d0216b537290fbd3
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-22 13:29:37 -0700

    LC-max-area-of-island [Runtime: 11 ms; Memory: 18.2 MB]

--- breadth-first-search/max-area-of-island.py
+++ breadth-first-search/max-area-of-island.py
@@ -6,11 +6,10 @@ class Solution:
             if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] != 1:
                 return 0
             grid[i][j] = 0
-            return 1 + dfs(i+1,j) + dfs(i-1,j) + dfs(i, j+1) + dfs(i,j-1)
-
-        max_island = 0
+            return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
+        area_island = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 1:
-                    max_island = max(max_island, dfs(i,j))
-        return max_island
+                    area_island = max(area_island, dfs(i,j))
+        return area_island
