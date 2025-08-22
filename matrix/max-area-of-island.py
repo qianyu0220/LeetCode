43d62bf46a210570eaf44825719e2d8662b0ceb8
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-22 13:36:32 -0700

    LC-max-area-of-island [Runtime: 8 ms; Memory: 18.3 MB]

--- matrix/max-area-of-island.py
+++ matrix/max-area-of-island.py
@@ -6,7 +6,7 @@ class Solution:
             if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] != 1:
                 return 0
             grid[i][j] = 0
-            return 1 + dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
+            return 1 + dfs(i+1,j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
         area_island = 0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
