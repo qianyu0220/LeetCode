e575f3097513fad371a20133767a26a575b3359b
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-22 22:05:32 -0700

    LC-3sum [Runtime: N/A; Memory: N/A]

--- array/3sum.py
+++ array/3sum.py
@@ -2,22 +2,22 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
         output = []
-        nums.sort()
-
+        nums = sorted(nums)
         for i in range(n):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            j, k = i+1, n-1
-
+            j = i + 1
+            k = n - 1
             while j < k:
                 total = nums[i] + nums[j] + nums[k]
-                if total > 0:
-                    k -= 1
-                elif total < 0:
-                    j += 1
-                else:
+                if total == 0:
                     output.append([nums[i], nums[j], nums[k]])
                     j += 1
                     while j < k and nums[j] == nums[j-1]:
-                        j += 1
+                        continue
+
+                elif total < 0:
+                    j += 1
+                else:
+                    k -= 1
         return output
