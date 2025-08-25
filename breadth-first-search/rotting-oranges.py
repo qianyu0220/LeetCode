1dd95e253f7bb5ae4a701623bab6c18722c187b6
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-24 20:19:52 -0700

    LC-rotting-oranges [Runtime: 3 ms; Memory: 17.8 MB]

--- breadth-first-search/rotting-oranges.py
+++ breadth-first-search/rotting-oranges.py
@@ -1,22 +1,22 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         rows, cols = len(grid), len(grid[0])
-        queue = deque()
         fresh = 0
+        queue = deque()
+        minutes = 0
         for i in range(rows):
             for j in range(cols):
-                if grid[i][j] == 2:
-                    queue.append((i, j, 0))
-                elif grid[i][j] == 1:
+                if grid[i][j] == 1:
                     fresh += 1
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
-        minutes = 0
+                elif grid[i][j] == 2:
+                    queue.append((i, j, 0))
+        directions = [(0, 1), (0, -1), (1, 0), (-1, 0)]
         while queue:
             r, c, minutes = queue.popleft()
             for dr, dc in directions:
                 nr, nc = r + dr, c + dc
-                if 0 <= nr < rows and 0 <= nc < cols and grid[nr][nc] == 1:
+                if nr>=0 and nr<rows and nc>=0 and nc<cols and grid[nr][nc] == 1:
                     grid[nr][nc] = 2
                     fresh -= 1
-                    queue.append((nr, nc, minutes + 1))
-        return -1 if fresh>0 else minutes
+                    queue.append((nr, nc, minutes+1))
+        return -1 if fresh > 0 else minutes
