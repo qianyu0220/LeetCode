3c50d02777bcedc112b7fd4eeb28cbaf870782c1
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-26 16:25:53 -0700

    LC-compare-version-numbers [Runtime: 0 ms; Memory: 18 MB]

--- string/compare-version-numbers.py
+++ string/compare-version-numbers.py
@@ -3,13 +3,12 @@ class Solution:
         v1 = version1.split(".")
         v2 = version2.split(".")
         n = max(len(v1), len(v2))
-        v1 += ["0"] * (n - len(v1))
-        v2 += ["0"] * (n - len(v2))
-
+        v1 += ["0"] *(n - len(v1))
+        v2 += ["0"] *(n - len(v2))
         for i in range(n):
             num1, num2 = int(v1[i]), int(v2[i])
-            if num1 > num2:
-                return 1
-            elif num1 < num2:
+            if num1 < num2:
                 return -1
+            elif num1 > num2:
+                return 1
         return 0
