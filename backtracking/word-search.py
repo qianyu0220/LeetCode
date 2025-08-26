4d339cc1bd2ae57b0d6512db86a8538a60e968cc
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-26 04:36:59 -0700

    LC-word-search [Runtime: 3100 ms; Memory: 18.1 MB]

--- backtracking/word-search.py
+++ backtracking/word-search.py
@@ -2,19 +2,18 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         rows = len(board)
         cols = len(board[0])
-
         def dfs(i, j, index):
             if index == len(word):
                 return True
-            if i<0 or i>=rows or j<0 or j>=cols or board[i][j] != word[index]:
+            if i<0 or i>=rows or j<0 or j>=cols or board[i][j]!=word[index]:
                 return False
-            temp = board[i][j]
+            tmp = board[i][j]
             board[i][j] = "#"
-            found = (dfs(i+1,j, index+1) or dfs(i-1,j, index+1) or dfs(i,j-1, index+1) or dfs(i,j+1, index+1))
-            board[i][j] = temp
+            found = (dfs(i+1,j,index+1) or dfs(i-1,j,index+1) or dfs(i,j+1,index+1) or dfs(i,j-1,index+1))
+            board[i][j] = tmp
             return found
         for i in range(rows):
             for j in range(cols):
-                if dfs(i, j, 0):
+                if dfs(i,j,0):
                     return True
         return False
