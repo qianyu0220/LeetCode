9e4e98cba7fb92fb265e394dc577c467a23ca3d5
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-25 20:33:29 -0700

    LC-permutations-ii [Runtime: 7 ms; Memory: 18.1 MB]

--- array/permutations-ii.py
+++ array/permutations-ii.py
@@ -1,22 +1,24 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        result = []
         nums = sorted(nums)
+        output = []
         used = [False] * len(nums)
+
         def backtrack(path):
             if len(path) == len(nums):
-                result.append(path[:])
+                output.append(path[:])
                 return
             for i in range(len(nums)):
-                if used[i]:
-                    continue
                 if i>0 and nums[i] == nums[i-1] and not used[i-1]:
                     continue
-                
+                if used[i]:
+                    continue
                 used[i] = True
                 path.append(nums[i])
                 backtrack(path)
                 path.pop()
                 used[i] = False
+
         backtrack([])
-        return result        
+        return output
+            
