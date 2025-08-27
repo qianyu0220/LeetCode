d6435a6952f2ddf166312215829a52571c41bce5
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-27 15:11:54 -0700

    LC-top-k-frequent-elements [Runtime: 8 ms; Memory: 21.3 MB]

--- heap-priority-queue/top-k-frequent-elements.py
+++ heap-priority-queue/top-k-frequent-elements.py
@@ -2,8 +2,9 @@ class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         count = Counter(nums)
         heap = []
+        n = len(nums)
         for num, freq in count.items():
             heapq.heappush(heap, (freq, num))
             if len(heap) > k:
                 heapq.heappop(heap)
-        return [num for freq, num in heap]
+        return [num for _, num in heap]
