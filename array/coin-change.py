8d0e38bfbfcf64b83e018b225d807bb43f26fe96
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-27 15:48:57 -0700

    LC-coin-change [Runtime: 712 ms; Memory: 18.2 MB]

--- array/coin-change.py
+++ array/coin-change.py
@@ -5,5 +5,5 @@ class Solution:
         for i in range(1, amount+1):
             for coin in coins:
                 if i >= coin:
-                    dp[i] = min(dp[i], dp[i-coin] + 1)
+                    dp[i] = min(dp[i], dp[i-coin]+1)
         return dp[amount] if dp[amount] != float("inf") else -1
