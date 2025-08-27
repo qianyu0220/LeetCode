4fdaf021f370fbeda56e1d4974195b5146923b66
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-27 15:50:55 -0700

    LC-coin-change [Runtime: 705 ms; Memory: 18.3 MB]

--- dynamic-programming/coin-change.py
+++ dynamic-programming/coin-change.py
@@ -4,6 +4,6 @@ class Solution:
         dp[0] = 0
         for i in range(1, amount+1):
             for coin in coins:
-                if i >= coin:
+                if i>=coin:
                     dp[i] = min(dp[i], dp[i-coin]+1)
         return dp[amount] if dp[amount] != float("inf") else -1
