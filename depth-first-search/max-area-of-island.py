26e24d30b16c201fd27abe21b451d09fb48e50d6
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-27 09:59:54 -0700

    LC-max-area-of-island [Runtime: N/A; Memory: N/A]

--- depth-first-search/max-area-of-island.py
+++ depth-first-search/max-area-of-island.py
@@ -1,15 +1,20 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
+        max_area = 0
+        rows, cols = len(grid), len(grid[0])
         def dfs(i, j):
-            if i<0 or i>=len(grid) or j<0 or j>=len(grid[0]) or grid[i][j] != 1:
-                return 0
+            if i<0 or i>=rows or j<0 or j>=cols or grid[i][j] != 1:
+                return
             grid[i][j] = 0
-            return 1 + dfs(i+1,j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1)
-        area_island = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
+            dfs(i+1, j)
+            dfs(i-1, j)
+            dfs(i, j+1)
+            dfs(i, j-1)
+        area = 0
+        for i in range(rows):
+            for j in range(cols):
                 if grid[i][j] == 1:
-                    area_island = max(area_island, dfs(i,j))
-        return area_island
+                    dfs(i, j)
+                    area += 1
+                    max_area = max(max_area, area)
+        return max_area
