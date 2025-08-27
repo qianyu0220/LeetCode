0b3514f6daad9d4f1ebef619161da4eae72917fa
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-27 10:19:46 -0700

    LC-sqrtx [Runtime: 6 ms; Memory: 17.9 MB]

--- math/sqrtx.py
+++ math/sqrtx.py
@@ -1,9 +1,8 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        left = 1
+        left = 0
         right = x
-        if x == 0:
-            return 0
+        output = 0
         while left <= right:
             mid = (left+right) // 2
             if mid * mid == x:
