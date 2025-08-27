89b79b2c50e279b9299031ed654174992d0207a2
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-27 10:02:28 -0700

    LC-max-area-of-island [Runtime: 13 ms; Memory: 18 MB]

--- depth-first-search/max-area-of-island.py
+++ depth-first-search/max-area-of-island.py
@@ -4,17 +4,11 @@ class Solution:
         rows, cols = len(grid), len(grid[0])
         def dfs(i, j):
             if i<0 or i>=rows or j<0 or j>=cols or grid[i][j] != 1:
-                return
+                return 0
             grid[i][j] = 0
-            dfs(i+1, j)
-            dfs(i-1, j)
-            dfs(i, j+1)
-            dfs(i, j-1)
-        area = 0
+            return 1+dfs(i+1, j)+dfs(i-1, j)+dfs(i, j+1)+dfs(i, j-1)
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 1:
-                    dfs(i, j)
-                    area += 1
-                max_area = max(max_area, area)
+                    max_area = max(max_area, dfs(i,j))
         return max_area
