8da943d1f5b47a3e61f39c5758ed18c9d00dae8e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-27 10:28:45 -0700

    LC-valid-parenthesis-string [Runtime: 0 ms; Memory: 17.7 MB]

--- dynamic-programming/valid-parenthesis-string.py
+++ dynamic-programming/valid-parenthesis-string.py
@@ -1,17 +1,17 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
         low = high = 0
-        for c in s:
-            if c == "(":
-                low += 1
-                high += 1
-            elif c == ")":
+        for i in range(len(s)):
+            if s[i] == "(":
+                low+=1
+                high+=1
+            elif s[i] == ")":
                 low -= 1
                 high -= 1
-            elif c == "*":
+            elif s[i] == "*":
                 low -= 1
                 high += 1
             if high < 0:
                 return False
-            low = max(low, 0)
+            low = max(0, low)
         return low == 0
