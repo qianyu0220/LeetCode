7205f38389d71bf943a13950453c366b6cb2747f
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-28 10:11:53 -0700

    LC-word-search [Runtime: 3113 ms; Memory: 17.7 MB]

--- array/word-search.py
+++ array/word-search.py
@@ -3,10 +3,11 @@ class Solution:
         rows = len(board)
         cols = len(board[0])
         def dfs(i, j, index):
-            if index == len(word):
+            if len(word) == index:
                 return True
             if i<0 or i>=rows or j<0 or j>=cols or board[i][j]!=word[index]:
                 return False
+            tmp = ""
             tmp = board[i][j]
             board[i][j] = "#"
             found = (dfs(i+1,j,index+1) or dfs(i-1,j,index+1) or dfs(i,j+1,index+1) or dfs(i,j-1,index+1))
