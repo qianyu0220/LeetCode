facd4fb5442521f173031b92f50aeb098751db9a
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-27 17:42:01 -0700

    LC-compare-version-numbers [Runtime: 0 ms; Memory: 17.7 MB]

--- string/compare-version-numbers.py
+++ string/compare-version-numbers.py
@@ -2,14 +2,15 @@ class Solution:
     def compareVersion(self, version1: str, version2: str) -> int:
         v1 = version1.split(".")
         v2 = version2.split(".")
+
         n = max(len(v1), len(v2))
         v1 += ["0"] * (n - len(v1))
         v2 += ["0"] * (n - len(v2))
 
         for i in range(n):
             num1, num2 = int(v1[i]), int(v2[i])
-            if num1 < num2:
-                return -1
-            elif num1 > num2:
+            if num1 > num2:
                 return 1
+            elif num1 < num2:
+                return -1
         return 0
