56704a156bbe9e76e01227b388abb890fca959db
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-28 10:28:16 -0700

    LC-rotting-oranges [Runtime: N/A; Memory: N/A]

--- array/rotting-oranges.py
+++ array/rotting-oranges.py
@@ -1,24 +1,24 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh = 0
-        minutes = 0
         rows = len(grid)
         cols = len(grid[0])
+        fresh = 0
         queue = deque()
+
         for i in range(rows):
             for j in range(cols):
                 if grid[i][j] == 1:
                     fresh += 1
                 elif grid[i][j] == 2:
                     queue.append((i, j, 0))
-        directions = [(1,0), (-1, 0), (0,1), (0,-1)]
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
         while queue:
             r, c, minutes = queue.popleft()
             for dr, dc in directions:
-                nr = r+dr
-                nc = c+dc
+                
+                nr, nc = dr+r, dc+c
                 if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]==1:
-                    grid[nr][nc] = 2
                     fresh -= 1
-                    queue.append((nr, nc, minutes+1))
-        return -1 if fresh!=0 else minutes
+                    grid[nr][nc] = 2
+                    queue.append((nr,nc,minutes+1))
+        return -1 if fresh>0 else minutes
