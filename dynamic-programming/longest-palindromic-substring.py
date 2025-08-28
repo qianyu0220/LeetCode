e97d7e953f8f5a88e249f00884795337173bfb35
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-28 09:16:39 -0700

    LC-longest-palindromic-substring [Runtime: 255 ms; Memory: 18.1 MB]

---
+++ dynamic-programming/longest-palindromic-substring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(l, r):
+            while l>=0 and r<len(s) and s[l]==s[r]:
+                l -= 1
+                r += 1
+            return s[l+1:r]
+        
+        result = ""
+        for i in range(len(s)):
+            odd = expand(i,i)
+            even = expand(i, i+1)
+            if len(odd) > len(result):
+                result = odd
+            if len(even) > len(result):
+                result = even
+        return result
