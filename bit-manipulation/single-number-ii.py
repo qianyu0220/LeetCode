a207016333fe74ff32606890db3e584569482291
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-08-31 14:22:24 -0700

    LC-single-number-ii [Runtime: 2 ms; Memory: 19.6 MB]

--- bit-manipulation/single-number-ii.py
+++ bit-manipulation/single-number-ii.py
@@ -1,8 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
         count = defaultdict(int)
-        for x in nums:
-            count[x] += 1
-        for x, freq in count.items():
+        for num in nums:
+            count[num] += 1
+        for num, freq in count.items():
             if freq == 1:
-                return x
+                return num
