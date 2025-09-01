e85fcb55a3992e2f1028659a8d05fe79c2c4295c
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-01 13:12:27 -0700

    LC-symmetric-tree [Runtime: 0 ms; Memory: 17.7 MB]

--- breadth-first-search/symmetric-tree.py
+++ breadth-first-search/symmetric-tree.py
@@ -9,10 +9,9 @@ class Solution:
         def check(node1, node2):
             if not node1 and not node2:
                 return True
-            elif not node1 or not node2:
+            if not node1 or not node2:
                 return False
             if node1.val != node2.val:
                 return False
-
             return check(node1.left, node2.right) and check(node1.right, node2.left)
         return check(root, root)
