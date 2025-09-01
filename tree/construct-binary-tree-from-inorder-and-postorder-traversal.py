bbc96d6afabb05282f125767cf5fcde296525faa
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-01 13:39:18 -0700

    LC-construct-binary-tree-from-inorder-and-postorder-traversal [Runtime: 59 ms; Memory: 54.1 MB]

---
+++ tree/construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, inorder: List[int], postorder: List[int]) -> Optional[TreeNode]:
+        postorder = deque(postorder)
+
+        def build(inorder):
+            if inorder:
+                idx = inorder.index(postorder.pop())
+                root = TreeNode(inorder[idx])
+                root.right = build(inorder[idx+1:])
+                root.left = build(inorder[:idx])
+                return root
+        return build(inorder)
+
+        
