4b5eab03af12a475b75caa71324616431b9c42ca
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-01 19:07:40 -0700

    LC-flatten-binary-tree-to-linked-list [Runtime: 3 ms; Memory: 18.1 MB]

---
+++ linked-list/flatten-binary-tree-to-linked-list.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root:
+            return None
+        self.flatten(root.left)
+        self.flatten(root.right)
+        if root.left:
+            temp = root.right
+            root.right = root.left
+            root.left = None
+
+            cur = root.right
+            while cur.right:
+                cur = cur.right
+            cur.right = temp
