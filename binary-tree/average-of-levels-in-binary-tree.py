b910d8b7553470302a97dfded13cc4744ad04eba
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-04 11:35:10 -0700

    LC-average-of-levels-in-binary-tree [Runtime: 4 ms; Memory: 20.1 MB]

---
+++ binary-tree/average-of-levels-in-binary-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        queue = deque([root])
+        result = []
+        while queue:
+            s = 0
+            lenq = len(queue)
+            for _ in range(lenq):
+                node = queue.popleft()
+                s += node.val
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            result.append(s/lenq)
+        return result
+            
