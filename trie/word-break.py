1a75124cac1eeb8c57c439ff3858a521d4a2a814
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-05 10:52:06 -0700

    LC-word-break [Runtime: 7 ms; Memory: 17.9 MB]

--- trie/word-break.py
+++ trie/word-break.py
@@ -3,7 +3,6 @@ class Solution:
         n = len(s)
         dp = [False] * (n+1)
         dp[0] = True
-
         for i in range(1, n+1):
             for j in range(i):
                 if dp[j] and s[j:i] in wordDict:
