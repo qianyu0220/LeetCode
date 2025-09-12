07ecc9268d56e66f3ad12dd7b13bba87e9a7b347
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-11 20:02:26 -0700

    LC-search-insert-position [Runtime: 0 ms; Memory: 18.6 MB]

--- binary-search/search-insert-position.py
+++ binary-search/search-insert-position.py
@@ -4,10 +4,10 @@ class Solution:
         right = len(nums) - 1
         while left <= right:
             mid = (left + right) // 2
-            if target == nums[mid]:
+            if nums[mid] == target:
                 return mid
-            elif target < nums[mid]:
-                right = mid - 1
-            else:
+            elif nums[mid] < target:
                 left = mid + 1
+            else:
+                right = mid - 1
         return left
