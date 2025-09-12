f4c01e3cdd822182c5c0f8b2b48a356f02156219
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-11 20:21:22 -0700

    LC-search-a-2d-matrix [Runtime: 0 ms; Memory: 18.2 MB]

--- binary-search/search-a-2d-matrix.py
+++ binary-search/search-a-2d-matrix.py
@@ -1,9 +1,17 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        n = len(matrix[0])
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == target:
-                    return True
+        if not matrix:
+            return False
+        m, n = len(matrix), len(matrix[0])
+        left, right = 0, m * n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            val = matrix[mid // n][mid % n]
+
+            if val == target:
+                return True
+            elif val < target:
+                left = mid + 1
+            else:
+                right = mid - 1
         return False
