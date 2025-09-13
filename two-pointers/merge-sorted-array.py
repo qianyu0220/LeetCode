b61dcbbe404cfe8d1609337c7911428d27407f06
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-13 14:00:24 -0700

    LC-merge-sorted-array [Runtime: 0 ms; Memory: 17.9 MB]

--- two-pointers/merge-sorted-array.py
+++ two-pointers/merge-sorted-array.py
@@ -3,24 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        p1 = 0
-        p2 = 0
+        p = 0
+        q = 0
         output = []
-        while p1 < m and p2 < n:
-            if nums1[p1] <= nums2[p2]:
-                output.append(nums1[p1])
-                p1 += 1
+        while p < m and q < n:
+            if nums1[p] <= nums2[q]:
+                output.append(nums1[p])
+                p += 1
             else:
-                output.append(nums2[p2])
-                p2 += 1
-        while p1 < m:
-            output.append(nums1[p1])
-            p1 += 1
-        while p2 < n:
-            output.append(nums2[p2])
-            p2 += 1
-
-        for i in range(len(output)):
+                output.append(nums2[q])
+                q += 1
+        while p < m:
+            output.append(nums1[p])
+            p += 1
+        while q < n:
+            output.append(nums2[q])
+            q += 1
+            
+        for i in range(m + n):
             nums1[i] = output[i]
-
-        
+        return nums1
