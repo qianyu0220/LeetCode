1b91923d91bea7191916a79ac7653b379b66e013
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-18 13:08:28 -0700

    LC-linked-list-cycle [Runtime: N/A; Memory: N/A]

--- linked-list/linked-list-cycle.py
+++ linked-list/linked-list-cycle.py
@@ -6,10 +6,6 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow_pointer = head
-        fast_pointer = head
-        while fast_pointer and fast_pointer.next:
-            slow_pointer = slow_pointer.next
-            fast_pointer = fast_pointer.next.next
-            if slow_pointer == fast_pointer:
-                return True
+        n = len(head)
+        if n > 0:
+            return True else False
