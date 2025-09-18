1a520924d896d70e84e9a00016db4f4fbeb057f2
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-18 13:22:37 -0700

    LC-maximum-product-subarray [Runtime: N/A; Memory: N/A]

--- array/maximum-product-subarray.py
+++ array/maximum-product-subarray.py
@@ -2,8 +2,8 @@ class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         n = len(nums)
         output = -float("inf")
-        if n == 1:
-            return nums[0]
         for i in range(1, n):
+            if nums[i] == 0:
+                continue
             output = max(output, nums[i] * nums[i-1])
         return output
