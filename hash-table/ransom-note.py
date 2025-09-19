641b40d767c39bb2d1fea2b2d6ed59fe0d6b3bd2
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-18 20:31:11 -0700

    LC-ransom-note [Runtime: 11 ms; Memory: 18.2 MB]

--- hash-table/ransom-note.py
+++ hash-table/ransom-note.py
@@ -2,7 +2,7 @@ class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
         r = collections.Counter(ransomNote)
         m = collections.Counter(magazine)
-        if r&m == r:
+        if r & m == r:
             return True
         else:
             return False
