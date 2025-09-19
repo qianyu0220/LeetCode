7afc62225f4ac485cfa5901f846f9b8aaf45db9e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-18 20:37:32 -0700

    LC-valid-anagram [Runtime: 15 ms; Memory: 18.5 MB]

--- sorting/valid-anagram.py
+++ sorting/valid-anagram.py
@@ -1,5 +1,5 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        s_sorted = sorted(s)
-        t_sorted = sorted(t)
-        return True if s_sorted == t_sorted else False
+        s = sorted(s)
+        t = sorted(t)
+        return True if s == t else False
