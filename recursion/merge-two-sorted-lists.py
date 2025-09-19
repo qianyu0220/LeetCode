4b1bff4c0e8e904dac84578635011841693ad565
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-18 20:12:58 -0700

    LC-merge-two-sorted-lists [Runtime: 0 ms; Memory: 17.9 MB]

--- recursion/merge-two-sorted-lists.py
+++ recursion/merge-two-sorted-lists.py
@@ -11,12 +11,10 @@ class Solution:
             if list1.val < list2.val:
                 cur.next = list1
                 list1 = list1.next
-                
             else:
                 cur.next = list2
                 list2 = list2.next
             cur = cur.next
-                
         if list1 or list2:
             cur.next = list1 if list1 else list2
         return dummy.next
