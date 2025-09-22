360448adcfe93d0e1088d7efc23df2472aa35acf
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-21 20:27:33 -0700

    LC-isomorphic-strings [Runtime: 7 ms; Memory: 18.1 MB]

--- hash-table/isomorphic-strings.py
+++ hash-table/isomorphic-strings.py
@@ -1,8 +1,22 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        # if len(s) != len(t):
+        #     return False
+        # s_to_t, t_to_s = {}, {}
+        # for char_s, char_t in zip(s, t):
+        #     if char_s in s_to_t:
+        #         if s_to_t[char_s] != char_t:
+        #             return False
+        #     s_to_t[char_s] = char_t
+        #     if char_t in t_to_s:
+        #         if t_to_s[char_t] != char_s:
+        #             return False
+        #     t_to_s[char_t] = char_s
+
+        # return True
+        s_to_t, t_to_s= {}, {}
         if len(s) != len(t):
             return False
-        s_to_t, t_to_s = {}, {}
         for char_s, char_t in zip(s, t):
             if char_s in s_to_t:
                 if s_to_t[char_s] != char_t:
@@ -12,6 +26,4 @@ class Solution:
                 if t_to_s[char_t] != char_s:
                     return False
             t_to_s[char_t] = char_s
-
-        return True
-                
+        return True                
