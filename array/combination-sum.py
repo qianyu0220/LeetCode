9d9f3ab2f8e1f2f1d80cf983737e53931fb58934
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-22 15:30:02 -0700

    LC-combination-sum [Runtime: N/A; Memory: N/A]

---
+++ array/combination-sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        output = []
+        def backtrack(start, path, cur_sum):
+            if cur_sum == target:
+                output.append(path)
+                return
+            if cur_sum > target:
+                return
+            for i in range(start, len(candidates)):
+                path.append(candidates[i])
+                backtrack(i, path, cur_sum + candidats[i])
+                path.pop()
+        backtrack(0, [], 0)
+        return output
