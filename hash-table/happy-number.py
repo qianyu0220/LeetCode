34839dccbc8c0a1db1765dacd7dc3b755afe9dd8
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-21 20:19:13 -0700

    LC-happy-number [Runtime: N/A; Memory: N/A]

---
+++ hash-table/happy-number.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        visit = set()
+        def sum_of_n(n):
+            output = 0
+            digit = n % 10
+            output += digit ** 2
+            n = n // 10
+            return output
+        while n not in visit:
+            visit.add(n)
+            n = sum_of_n(n)
+            if n == 1:
+                return True
+        return False
