74a6a08789fbb6aff2f01aaecda6f6e7373d6caf
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-22 10:46:10 -0700

    LC-maximum-sum-circular-subarray [Runtime: 108 ms; Memory: 21.3 MB]

--- dynamic-programming/maximum-sum-circular-subarray.py
+++ dynamic-programming/maximum-sum-circular-subarray.py
@@ -1,16 +1,13 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: List[int]) -> int:
-        total_sum = sum(nums)
         n = len(nums)
-        max_sum = nums[0]
-        min_sum = nums[0]
-        max_cur = nums[0]
-        min_cur = nums[0]
+        max_cur, min_cur, max_sum, min_sum = nums[0],nums[0],nums[0],nums[0]
+        total = sum(nums)
         for i in range(1, n):
-            max_cur = max(nums[i], max_cur + nums[i])
-            min_cur = min(nums[i], min_cur + nums[i])
+            max_cur = max(nums[i], max_cur+nums[i])
+            min_cur = min(nums[i], min_cur+nums[i])
             max_sum = max(max_sum, max_cur)
             min_sum = min(min_sum, min_cur)
         if max_sum < 0:
             return max_sum
-        return max(max_sum, total_sum - min_sum)
+        return max(max_sum, total-min_sum)
