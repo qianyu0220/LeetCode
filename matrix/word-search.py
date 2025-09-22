2b689d6f5cbaa5b4b512b0ac4e12d38e5be95312
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-22 13:35:19 -0700

    LC-word-search [Runtime: 3046 ms; Memory: 18 MB]

--- matrix/word-search.py
+++ matrix/word-search.py
@@ -1,20 +1,40 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        # rows = len(board)
+        # cols = len(board[0])
+        # def dfs(i, j, index):
+        #     if len(word) == index:
+        #         return True
+        #     if i<0 or i>=rows or j<0 or j>=cols or board[i][j]!=word[index]:
+        #         return False
+        #     tmp = ""
+        #     tmp = board[i][j]
+        #     board[i][j] = "#"
+        #     found = (dfs(i+1,j,index+1) or dfs(i-1,j,index+1) or dfs(i,j+1,index+1) or dfs(i,j-1,index+1))
+        #     board[i][j] = tmp
+        #     return found
+        # for i in range(rows):
+        #     for j in range(cols):
+        #         if dfs(i,j,0):
+        #             return True
+        # return False
+
         rows = len(board)
         cols = len(board[0])
         def dfs(i, j, index):
             if len(word) == index:
                 return True
-            if i<0 or i>=rows or j<0 or j>=cols or board[i][j]!=word[index]:
+            if i < 0 or i >= rows or j < 0 or j >= cols or board[i][j] != word[index]:
                 return False
             tmp = ""
             tmp = board[i][j]
             board[i][j] = "#"
-            found = (dfs(i+1,j,index+1) or dfs(i-1,j,index+1) or dfs(i,j+1,index+1) or dfs(i,j-1,index+1))
+            found = (dfs(i+1, j, index+1) or dfs(i-1, j, index+1) or dfs(i, j+1, index+1) or dfs(i, j-1, index+1))
             board[i][j] = tmp
             return found
+        
         for i in range(rows):
             for j in range(cols):
-                if dfs(i,j,0):
+                if dfs(i, j, 0):
                     return True
         return False
