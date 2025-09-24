64727dcbac6ee4e50209b6eeb3942cce91c508c9
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-24 13:27:01 -0700

    LC-house-robber [Runtime: 0 ms; Memory: 17.8 MB]

--- array/house-robber.py
+++ array/house-robber.py
@@ -1,10 +1,10 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1:
-            return nums[0]
         if n == 0:
             return 0
+        if n == 1:
+            return nums[0]
         dp = [nums[0]] * n
         dp[0] = nums[0]
         dp[1] = max(nums[0], nums[1])
