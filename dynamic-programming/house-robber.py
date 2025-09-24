477749a437e5c7ddc4ac8af8a78a0c140c5c69ea
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-24 13:12:43 -0700

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- dynamic-programming/house-robber.py
+++ dynamic-programming/house-robber.py
@@ -2,6 +2,8 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
         dp = [nums[0]] * n
+        if n == 1:
+            return nums[0]
         dp[0] = nums[0]
         dp[1] = nums[1]
         for i in range(2, n):
