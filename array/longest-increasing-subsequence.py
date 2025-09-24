381dd56970b6862271ac5449193f7787ccf9ab1b
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-24 14:26:25 -0700

    LC-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

--- array/longest-increasing-subsequence.py
+++ array/longest-increasing-subsequence.py
@@ -1,11 +1,19 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # n = len(nums)
+        # dp = [1] * n
+        # if not nums:
+        #     return 0
+        # for i in range(1, n):
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             dp[i] = max(dp[j]+1, dp[i])
+        # return max(dp)
         n = len(nums)
         dp = [1] * n
-        if not nums:
-            return 0
+        output = 1
         for i in range(1, n):
             for j in range(i):
                 if nums[i] > nums[j]:
-                    dp[i] = max(dp[j]+1, dp[i])
-        return max(dp)
+                    dp[i] = max(dp[i], dp[j] + 1)
+        return dp[-1]
