384252f4e63001fa020c96d033a9832a16055117
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-23 19:48:23 -0700

    LC-combinations [Runtime: 142 ms; Memory: 59.7 MB]

--- backtracking/combinations.py
+++ backtracking/combinations.py
@@ -1,15 +1,14 @@
 class Solution:
     def combine(self, n: int, k: int) -> List[List[int]]:
-        result = []
+        output = []
         path = []
-
         def backtrack(start):
             if len(path) == k:
-                result.append(path[:])
-                return
+                output.append(path[:])
+                return 
             for i in range(start, n+1):
                 path.append(i)
                 backtrack(i+1)
                 path.pop()
         backtrack(1)
-        return result
+        return output
