d2abfa60b7fd3d9e89f2ecb44c18cec4fb24375d
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-24 13:12:08 -0700

    LC-house-robber [Runtime: N/A; Memory: N/A]

--- array/house-robber.py
+++ array/house-robber.py
@@ -1,13 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        if n==0:
-            return 0
-        if n==1:
-            return nums[0]
-        dp = [0] * n
+        dp = [nums[0]] * n
         dp[0] = nums[0]
-        dp[1] = max(dp[0], nums[1])
+        dp[1] = nums[1]
         for i in range(2, n):
-            dp[i] = max(dp[i-1], dp[i-2] + nums[i])
-        return dp[-1]
+            dp[i] = nums[i] + dp[i-2]
+        return max(dp[-1], dp[-2])
