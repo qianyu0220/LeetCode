40e3a7bb5d1f499516a00bc753456a9dda907660
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-25 15:34:12 -0700

    LC-maximal-square [Runtime: 127 ms; Memory: 33 MB]

--- array/maximal-square.py
+++ array/maximal-square.py
@@ -4,9 +4,8 @@ class Solution:
             return 0
         m = len(matrix)
         n = len(matrix[0])
-        dp = [[0] * (n+1) for _ in range(m+1)]
         output = 0
-
+        dp = [[0] * (n+1) for _ in range(m+1)]
         for i in range(1, m+1):
             for j in range(1, n+1):
                 if matrix[i-1][j-1] == "1":
