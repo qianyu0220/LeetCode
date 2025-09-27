91be9fd1c7c226dd88ab80f50c85c6eada4a96eb
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-27 09:54:22 -0700

    LC-rotate-image [Runtime: 0 ms; Memory: 17.7 MB]

--- array/rotate-image.py
+++ array/rotate-image.py
@@ -3,15 +3,9 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        # n = len(matrix)
-        # for i in range(n):
-        #     for j in range(i+1, n):
-        #         matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        # for row in matrix:
-        #     row.reverse()
         n = len(matrix)
         for i in range(n):
             for j in range(i+1, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for rows in matrix:
-            rows.reverse()
+        for row in matrix:
+            row.reverse()
