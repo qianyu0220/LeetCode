f300d4e641ac2787ebc29b6296f32c9a0523f9fb
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-26 18:12:41 -0700

    LC-valid-sudoku [Runtime: 7 ms; Memory: 18 MB]

--- array/valid-sudoku.py
+++ array/valid-sudoku.py
@@ -3,7 +3,7 @@ class Solution:
         result = []
         for i in range(9):
             for j in range(9):
-                element = board[i][j] 
+                element = board[i][j]
                 if element != ".":
-                    result += (i, element), (element, j), (i //3, j // 3, element)
+                    result += [(i, element), (element, j), (i // 3, j // 3, element)]
         return len(result) == len(set(result))
