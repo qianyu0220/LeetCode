f2a4cb89fbb58aa9c0bbd57b15adb222ef32d7e6
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-29 13:42:47 -0700

    LC-factorial-trailing-zeroes [Runtime: 0 ms; Memory: 17.6 MB]

--- math/factorial-trailing-zeroes.py
+++ math/factorial-trailing-zeroes.py
@@ -2,7 +2,7 @@ class Solution:
     def trailingZeroes(self, n: int) -> int:
         fives = 0
         i = 5
-        while i <= n:
-            n = n // 5
+        while n >= 5:
+            n //= 5
             fives += n
         return fives
