12102736154acd6104b083f2e0cfd61625742310
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-09-29 13:45:55 -0700

    LC-powx-n [Runtime: 0 ms; Memory: 17.7 MB]

--- math/powx-n.py
+++ math/powx-n.py
@@ -1,12 +1,12 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        result = 1
         if n < 0:
             n = -n
             x = 1/x
+        result = 1
         while n > 0:
             if n % 2 == 1:
-                result = result * x
+                result *= x
             n = n // 2
-            x *= x
+            x = x ** 2
         return result
