53a6f11f1b1c8f7c39907a23003354b709b2ee50
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-02 12:29:45 -0700

    LC-minimize-result-by-adding-parentheses-to-expression [Runtime: 3 ms; Memory: 17.8 MB]

--- string/minimize-result-by-adding-parentheses-to-expression.py
+++ string/minimize-result-by-adding-parentheses-to-expression.py
@@ -1,21 +1,20 @@
 class Solution:
     def minimizeResult(self, expression: str) -> str:
         n = len(expression)
-        smallest = float("inf")
-        output = ""
         place = 0
+        output = ""
+        smallest = float("inf")
         for i in range(n):
             if expression[i] == "+":
                 place = i
         for i in range(place):
-            for j in range(place + 1,n):
+            for j in range(place+1, n):
                 left = int(expression[:i]) if i>0 else 1
                 mid1 = int(expression[i:place])
                 mid2 = int(expression[place+1: j+1])
                 right = int(expression[j+1:]) if j+1 < n else 1
-
                 val = left * (mid1 + mid2) * right
                 if val < smallest:
                     smallest = val
-                    output = (expression[:i] + "(" + expression[i:j+1] + ")" + expression[j+1:])
+                    output = expression[:i] + "(" + expression[i: j+1] + ")" + expression[j+1:]
         return output
