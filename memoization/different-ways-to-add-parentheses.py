85a25eda5608a39a132b36248ccf1236aa1ffd8e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-02 09:45:20 -0700

    LC-different-ways-to-add-parentheses [Runtime: N/A; Memory: N/A]

---
+++ memoization/different-ways-to-add-parentheses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        result = []
+        for i in range(len(expression)):
+            oper = expression[i]
+            if oper == "+" or oper == "-" or oper == "*":
+                sub1 = expression[:i]
+                sub2 = expression[i+1:]
+                s1 = self.diffWaysToCompute(sub1)
+                s2 = self.diffWaysToCompute(sub2)
+                for i in s1:
+                    for j in s2:
+                        if oper == "+":
+                            result.append(int(i) * int(j))
+                        elif oper == "-":
+                            result.append(int(i) - int(j))
+                        else:
+                            result.append(int(i) * int(j))
+        if len(result) == 0:
+            result.append(int(expression))
+
+        return result
