f613123246d6a2b1048372822497e67b0a590086
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-02 20:30:41 -0700

    LC-unique-paths-ii [Runtime: 0 ms; Memory: 18 MB]

--- array/unique-paths-ii.py
+++ array/unique-paths-ii.py
@@ -1,5 +1,16 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        # m = len(obstacleGrid)
+        # n = len(obstacleGrid[0])
+        # dp = [0] * n
+        # dp[0] = 1 if obstacleGrid[0][0] == 0 else 0
+        # for i in range(m):
+        #     for j in range(n):
+        #         if obstacleGrid[i][j] == 1:
+        #             dp[j] = 0
+        #         elif j > 0:
+        #             dp[j] += dp[j-1]
+        # return dp[-1]
         m = len(obstacleGrid)
         n = len(obstacleGrid[0])
         dp = [0] * n
@@ -10,4 +21,4 @@ class Solution:
                     dp[j] = 0
                 elif j > 0:
                     dp[j] += dp[j-1]
-        return dp[-1]
+        return dp[-1] 
