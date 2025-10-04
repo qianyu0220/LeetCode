3993fb82cb6e66afbc27419e09646e9004127ef4
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-04 14:42:20 -0700

    LC-minimize-result-by-adding-parentheses-to-expression [Runtime: 2 ms; Memory: 17.7 MB]

--- enumeration/minimize-result-by-adding-parentheses-to-expression.py
+++ enumeration/minimize-result-by-adding-parentheses-to-expression.py
@@ -10,13 +10,11 @@ class Solution:
         for i in range(place):
             for j in range(place+1, n):
                 left = int(expression[:i]) if i > 0 else 1
-                mid1 = int(expression[i:place])
-                mid2 = int(expression[place + 1:j+1])
-                right = int(expression[j+1:]) if j+1 < n else 1
+                mid1 = int(expression[i: place])
+                mid2 = int(expression[place+1: j+1])
+                right = int(expression[j+1: n]) if j+1 < n else 1
                 val = left * (mid1 + mid2) * right
                 if val < smallest:
                     smallest = val
-                    output = (expression[:i] + "(" + expression[i:j+1] + ")" + expression[j+1:])
-        if not output:
-            output = expression
+                    output = (expression[:i] + "(" + expression[i: j+1] + ")" + expression[j+1: n])
         return output
