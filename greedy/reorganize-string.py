ee6b2bd0985b2261e8e91158c0314fe042568f09
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-04 18:41:09 -0700

    LC-reorganize-string [Runtime: N/A; Memory: N/A]

---
+++ greedy/reorganize-string.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        n = len(s)
+        output = ""
+        output += s[0]
+        for i in range(1, n-1):
+            if s[i] == s[i-1] == s[i+1]:
+                return ""
+            if s[i] == s[i-1] and s[i] != [i+1]:
+                output += s[i+1]
+                output += s[i]
+        return output
