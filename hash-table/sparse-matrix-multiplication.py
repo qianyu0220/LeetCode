33dc89e9aa509d8fe6bfc818223f7cc4ca6fc1cc
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-10 14:46:03 -0700

    LC-sparse-matrix-multiplication [Runtime: 0 ms; Memory: 18.5 MB]

--- hash-table/sparse-matrix-multiplication.py
+++ hash-table/sparse-matrix-multiplication.py
@@ -1,30 +1,29 @@
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
-        if not mat1 or not mat2 or not mat1[0] or not mat2[0]:
+        if not mat1 or not mat2:
             return []
         m = len(mat1)
         n = len(mat1[0])
         p = len(mat2[0])
-
-        A_rows = []
+        row_A = []
         for i in range(m):
             rows_dict = {}
             for k, val in enumerate(mat1[i]):
                 if val != 0:
                     rows_dict[k] = val
-            A_rows.append(rows_dict)
-        B_rows = []
+            row_A.append(rows_dict)
+        row_B = []
         for i in range(n):
             rows_dict = {}
             for k, val in enumerate(mat2[i]):
                 if val != 0:
                     rows_dict[k] = val
-            B_rows.append(rows_dict)
+            row_B.append(rows_dict)
         C = [[0] * p for _ in range(m)]
         for i in range(m):
-            for k, a in A_rows[i].items():
-                if not B_rows[k]:
+            for k, a in row_A[i].items():
+                if not row_B[k]:
                     continue
-                for j, b in B_rows[k].items():
-                    C[i][j] += a * b
+                for j, b in row_B[k].items():
+                    C[i][j] += a* b
         return C
