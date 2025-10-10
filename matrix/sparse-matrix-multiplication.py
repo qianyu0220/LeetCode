5a205167d392f44cab38bb3b6d5fc56d57a04d59
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-09 19:21:44 -0700

    LC-sparse-matrix-multiplication [Runtime: N/A; Memory: N/A]

---
+++ matrix/sparse-matrix-multiplication.py
@@ -0,0 +1,29 @@
+class Solution:
+    def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
+        m = len(mat1)
+        k = len(mat1[0])
+        p = len(mat2[0])
+        if not mat1 or not mat2 or not mat1[0] or not mat2[0]:
+            return []
+        A_rows = []
+        for i in range(m):
+            row_dict = {}
+            for k, val in enumerate(mat1[i]):
+                if val != 0:
+                    row_dict[k] = val
+            A_rows.append(row_dict)
+        B_rows = []
+        for i in range(p):
+            row_dict = {}
+            for j, val in enumerate(mat2[i]):
+                if val != 0:
+                    row_dict[j] = val
+            B_rows.append(row_dict)
+        C = [[0] * p for _ in range(m)]
+        for i in range(m):
+            for k, a in A_rows[i].items():
+                if not B_rows[k]:
+                    continue
+                for j, b in B_rows[k].items():
+                    C[i][j] += a*b
+        return C
