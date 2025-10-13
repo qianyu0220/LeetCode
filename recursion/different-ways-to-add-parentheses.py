a961bbf3be221f36520941a62396c026a3fca9b9
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-12 20:40:57 -0700

    LC-different-ways-to-add-parentheses [Runtime: 4 ms; Memory: 17.7 MB]

--- recursion/different-ways-to-add-parentheses.py
+++ recursion/different-ways-to-add-parentheses.py
@@ -1,9 +1,28 @@
 class Solution:
     def diffWaysToCompute(self, expression: str) -> List[int]:
+        # result = []
+        # for i in range(len(expression)):
+        #     oper = expression[i]
+        #     if oper == "+" or oper == "-" or oper == "*":
+        #         sub1 = expression[:i]
+        #         sub2 = expression[i+1:]
+        #         s1 = self.diffWaysToCompute(sub1)
+        #         s2 = self.diffWaysToCompute(sub2)
+        #         for l in s1:
+        #             for r in s2:
+        #                 if oper == "+":
+        #                     result.append(int(l) + int(r))
+        #                 elif oper == "-":
+        #                     result.append(int(l) - int(r))
+        #                 else:
+        #                     result.append(int(l) * int(r))
+        # if not result:
+        #     result.append(int(expression))
+        # return result
         result = []
         for i in range(len(expression)):
             oper = expression[i]
-            if oper == "+" or oper == "-" or oper == "*":
+            if oper == "+" or oper =="-" or oper == "*":
                 sub1 = expression[:i]
                 sub2 = expression[i+1:]
                 s1 = self.diffWaysToCompute(sub1)
