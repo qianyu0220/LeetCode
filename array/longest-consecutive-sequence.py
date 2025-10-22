8309401329ef3b93c4c7d5beb5539540a8989dff
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-22 15:43:03 -0700

    LC-longest-consecutive-sequence [Runtime: 35 ms; Memory: 34.3 MB]

--- array/longest-consecutive-sequence.py
+++ array/longest-consecutive-sequence.py
@@ -7,5 +7,5 @@ class Solution:
                 length = 0
                 while num + length in nums_set:
                     length += 1
-            longest = max(longest, length)
+                longest = max(longest, length)
         return longest
