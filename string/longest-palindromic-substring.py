bb33b1411b8e1e9833b5b7cec1fdc578a637233c
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-23 13:41:33 -0700

    LC-longest-palindromic-substring [Runtime: 259 ms; Memory: 18 MB]

--- string/longest-palindromic-substring.py
+++ string/longest-palindromic-substring.py
@@ -1,18 +1,18 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        if len(s) < 2:
-            return s
-        def expand(l, r):
+        n = len(s)
+        output = ""
+        def count(l,r):
             while l>=0 and r<len(s) and s[l] == s[r]:
                 l -= 1
                 r += 1
             return l+1, r
         start, end = 0, 1
         for i in range(len(s)):
-            l1, r1 = expand(i,i)
-            l2, r2 = expand(i, i+1)
+            l1, r1 = count(i, i)
+            l2, r2 = count(i, i+1)
             if r1 - l1 > end - start:
                 start, end = l1, r1
             if r2 - l2 > end - start:
                 start, end = l2, r2
-        return s[start:end]
+        return s[start: end]
