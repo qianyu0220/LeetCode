0e1604b0cbe8155e39f57c80c4eb3d15c3f14065
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-23 14:00:18 -0700

    LC-missing-number [Runtime: 11 ms; Memory: 18.6 MB]

--- math/missing-number.py
+++ math/missing-number.py
@@ -2,8 +2,7 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         nums.sort()
         n = len(nums)
-        if nums[-1] == n - 1:
-            return n
         for i, num in enumerate(nums):
             if num != i:
                 return i
+        return n
