9359faf2259e63cb842f7095a1f3bf59114ac0cd
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-23 12:31:39 -0700

    LC-palindromic-substrings [Runtime: 124 ms; Memory: 17.9 MB]

--- string/palindromic-substrings.py
+++ string/palindromic-substrings.py
@@ -1,14 +1,14 @@
 class Solution:
     def countSubstrings(self, s: str) -> int:
         output = 0
-        def count(s, left, right):
+        def count_palin(s, l, r):
             count = 0
-            while left >= 0 and right < len(s) and s[left] == s[right]:
-                left -= 1
-                right += 1
+            while l>=0 and r<len(s) and s[l] == s[r]:
+                l -= 1
+                r += 1
                 count += 1
             return count
         for i in range(len(s)):
-            output += count(s, i, i)
-            output += count(s, i, i+1)
+            output += count_palin(s, i, i)
+            output += count_palin(s, i, i+1)
         return output
