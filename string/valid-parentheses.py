3e7ad6a0be7283f5edd4c6564d047a1709e0cd80
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-24 15:43:36 -0700

    LC-valid-parentheses [Runtime: N/A; Memory: N/A]

--- string/valid-parentheses.py
+++ string/valid-parentheses.py
@@ -15,6 +15,6 @@ class Solution:
             if char in mapping.values():
                 stack.append(char)
             elif char in mapping.keys():
-                if not stack or mapping[char] ！= stack.pop():
+                if not stack or mapping[char] = stack.pop():
                     return False
         return not stack
