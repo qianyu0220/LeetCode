3a1199101a2d7a4ba8847757ed4d71912ad087b6
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-24 18:32:11 -0700

    LC-reorder-list [Runtime: 0 ms; Memory: 23.3 MB]

--- stack/reorder-list.py
+++ stack/reorder-list.py
@@ -15,16 +15,16 @@ class Solution:
             fast = fast.next.next
         second = slow.next
         slow.next = None
-        node = None
-
+        prev = None
         while second:
-            tmp = second.next
-            second.next = node
-            node = second
-            second = tmp
+            nxt = second.next
+            second.next = prev
+            prev = second
+            second = nxt
+        second = prev
         first = head
-        second = node
         while second:
             tmp1, tmp2 = first.next, second.next
-            first.next, second.next = second, tmp1
+            first.next = second
+            second.next = tmp1
             first, second = tmp1, tmp2
