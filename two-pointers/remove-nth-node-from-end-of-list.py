c5180eae8e0a98c4fa6f99babca1cde75a256154
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-24 19:34:10 -0700

    LC-remove-nth-node-from-end-of-list [Runtime: 0 ms; Memory: 17.8 MB]

--- two-pointers/remove-nth-node-from-end-of-list.py
+++ two-pointers/remove-nth-node-from-end-of-list.py
@@ -6,12 +6,12 @@
 class Solution:
     def removeNthFromEnd(self, head: Optional[ListNode], n: int) -> Optional[ListNode]:
         dummy = ListNode(0, head)
-        slow = dummy
-        fast = dummy
+        fast, slow = dummy, dummy
         for _ in range(n+1):
             fast = fast.next
         while fast:
             fast = fast.next
             slow = slow.next
+
         slow.next = slow.next.next
         return dummy.next
