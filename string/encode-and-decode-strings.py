6b2dfe0d5d0365070c23531a651807418fef4dd4
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-24 19:58:03 -0700

    LC-encode-and-decode-strings [Runtime: 78 ms; Memory: 18.2 MB]

---
+++ string/encode-and-decode-strings.py
@@ -0,0 +1,27 @@
+class Codec:
+    def encode(self, strs: List[str]) -> str:
+        """Encodes a list of strings to a single string.
+        """
+        result = ""
+        for s in strs:
+            result += str(len(s)) + "#" + s
+        return result
+    def decode(self, s: str) -> List[str]:
+        """Decodes a single string to a list of strings.
+        """
+        result = []
+        i = 0
+        while i < len(s):
+            j = i
+            while s[j] != "#":
+                j += 1
+            length = int(s[i:j])
+            i = j + 1
+            result.append(s[i:i+length])
+            i = i + length
+        return result
+
+
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.decode(codec.encode(strs))
