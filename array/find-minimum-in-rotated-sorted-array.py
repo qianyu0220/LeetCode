54a9f1e08b958093410ef146e501d65aab660bd4
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-26 14:38:44 -0700

    LC-find-minimum-in-rotated-sorted-array [Runtime: 0 ms; Memory: 18.1 MB]

--- array/find-minimum-in-rotated-sorted-array.py
+++ array/find-minimum-in-rotated-sorted-array.py
@@ -3,9 +3,9 @@ class Solution:
         left = 0
         right = len(nums) - 1
         while left < right:
-            mid = (left + right) // 2
+            mid = (left+right) // 2
             if nums[mid] > nums[right]:
                 left = mid + 1
             else:
-                right = mid 
-        return nums[left]
+                right = mid
+        return nums[right]
