a43c295d8c3133e22312b32e710b698942113a1a
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-26 14:01:07 -0700

    LC-merge-k-sorted-lists [Runtime: 8 ms; Memory: 20.2 MB]

--- merge-sort/merge-k-sorted-lists.py
+++ merge-sort/merge-k-sorted-lists.py
@@ -12,16 +12,14 @@ class Solution:
             for i in range(0, len(lists), 2):
                 l1 = lists[i]
                 l2 = lists[i+1] if i+1 < len(lists) else None
-                temp.append(self.merge_lists(l1, l2))
+                temp.append(self.merge_l1_l2(l1, l2))
             lists = temp
         return lists[0]
-
     
 
-    def merge_lists(self, l1, l2):
+    def merge_l1_l2(self, l1, l2):
         node = ListNode()
         result = node
-
         while l1 and l2:
             if l1.val < l2.val:
                 node.next = l1
@@ -33,3 +31,4 @@ class Solution:
         if l1 or l2:
             node.next = l1 if l1 else l2
         return result.next
+            
