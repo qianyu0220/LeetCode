8c920a702c3609c0571be6a18aa7a80855a1040e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-27 14:07:21 -0700

    LC-meeting-rooms-ii [Runtime: 3 ms; Memory: 20.1 MB]

--- two-pointers/meeting-rooms-ii.py
+++ two-pointers/meeting-rooms-ii.py
@@ -2,9 +2,9 @@ class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
         starts = sorted([s for s, e in intervals])
         ends = sorted([e for s, e in intervals])
-        rooms = max_rooms = 0
         i = j = 0
-        while i<len(intervals) and j<len(intervals):
+        rooms = max_rooms = 0
+        while i < len(starts):
             if starts[i] < ends[j]:
                 rooms += 1
                 max_rooms = max(max_rooms, rooms)
