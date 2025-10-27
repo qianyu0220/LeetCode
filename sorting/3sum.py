5d2c873e8c952f4a3b91e021c10ed33ae7087235
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-27 10:39:34 -0700

    LC-3sum [Runtime: 559 ms; Memory: 20.7 MB]

--- sorting/3sum.py
+++ sorting/3sum.py
@@ -6,16 +6,16 @@ class Solution:
         for i in range(n):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
-            k, j = i + 1, n -1
+            k, j = i+1, n-1
             while k < j:
                 total = nums[i] + nums[k] + nums[j]
-                if total < 0:
-                    k += 1
-                elif total > 0:
+                if total > 0:
                     j -= 1
+                elif total < 0:
+                    k += 1
                 else:
                     output.append([nums[i], nums[k], nums[j]])
                     k += 1
                     while k < j and nums[k] == nums[k-1]:
                         k += 1
-        return output
+        return output 
