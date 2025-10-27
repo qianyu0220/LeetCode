9dc3f6565c9280109c9f0c833b56ca828ce10244
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-27 14:15:08 -0700

    LC-coin-change [Runtime: 712 ms; Memory: 18 MB]

--- breadth-first-search/coin-change.py
+++ breadth-first-search/coin-change.py
@@ -1,10 +1,9 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        n = len(coins)
-        dp = [float("inf")] * (amount+1)
+        dp = [float("inf")] * (amount + 1)
         dp[0] = 0
         for i in range(1, amount+1):
             for coin in coins:
-                if i >=coin:
-                    dp[i] = min(dp[i], dp[i-coin]+1)
+                if i>=coin:
+                    dp[i] = min(dp[i], dp[i-coin] + 1)
         return dp[-1] if dp[-1] != float("inf") else -1
