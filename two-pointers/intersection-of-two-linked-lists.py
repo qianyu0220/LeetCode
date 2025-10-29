44ec88e34d5bf99558b232944d09af53f7694696
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-29 14:31:37 -0700

    LC-intersection-of-two-linked-lists [Runtime: 83 ms; Memory: 27.6 MB]

--- two-pointers/intersection-of-two-linked-lists.py
+++ two-pointers/intersection-of-two-linked-lists.py
@@ -6,17 +6,9 @@
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        if not headA or not headB:
-            return None
         p1 = headA
         p2 = headB
-        while p1 != p2:
-            if p1:
-                p1 = p1.next
-            else:
-                p1 = headB
-            if p2:
-                p2 = p2.next 
-            else:
-                p2 = headA
+        while p1!=p2:
+            p1 = p1.next if p1 else headB
+            p2 = p2.next if p2 else headA
         return p1
