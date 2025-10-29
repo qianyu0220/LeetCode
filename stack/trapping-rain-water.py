645c2f134aef3d58adc74b8db9a880800b66224e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-29 13:59:19 -0700

    LC-trapping-rain-water [Runtime: 7 ms; Memory: 19.1 MB]

--- stack/trapping-rain-water.py
+++ stack/trapping-rain-water.py
@@ -1,9 +1,16 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
-        rain_water = 0
-        for i in range(n):
-            left_max = max(height[:i+1])
-            right_max = max(height[i:])
-            rain_water += min(left_max, right_max) - height[i]
-        return rain_water
+        left, right = 0, n-1
+        left_max, right_max = height[left], height[right]
+        water = 0
+        while left < right:
+            if left_max < right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
+            else:
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+        return water
