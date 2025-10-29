4660661c1d2a4850b21fe871dddad9daa19bc555
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-29 16:43:33 -0700

    LC-recover-binary-search-tree [Runtime: 5 ms; Memory: 18.1 MB]

--- tree/recover-binary-search-tree.py
+++ tree/recover-binary-search-tree.py
@@ -12,7 +12,7 @@ class Solution:
         self.first = self.second = self.prev = None
         def inorder(node):
             if not node:
-                return 
+                return
             inorder(node.left)
             if self.prev and self.prev.val > node.val:
                 if not self.first:
@@ -20,5 +20,6 @@ class Solution:
                 self.second = node
             self.prev = node
             inorder(node.right)
+
         inorder(root)
         self.first.val, self.second.val = self.second.val, self.first.val
