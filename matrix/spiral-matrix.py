fad11d950eb5e0ba46ef85f8b3b35ad7d038a59d
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-28 19:08:57 -0700

    LC-spiral-matrix [Runtime: 0 ms; Memory: 17.4 MB]

--- matrix/spiral-matrix.py
+++ matrix/spiral-matrix.py
@@ -3,21 +3,24 @@ class Solution:
         m = len(matrix)
         n = len(matrix[0])
         output = []
-        left, right = 0, n-1
         top, bottom = 0, m-1
-        while left <= right and top <= bottom:
+        left, right = 0, n-1
+        while top <= bottom and left <= right:
             for i in range(left, right+1):
                 output.append(matrix[top][i])
-            top += 1
+            top +=1
+            if top > bottom:
+                break
             for i in range(top, bottom+1):
                 output.append(matrix[i][right])
             right -= 1
-            if top <= bottom:
-                for i in range(right, left-1, -1):
-                    output.append(matrix[bottom][i])
-                bottom -= 1
-            if left <= right:
-                for i in range(bottom, top-1, -1):
-                    output.append(matrix[i][left])
-                left += 1
+            if left > right:
+                break
+            for i in range(right, left-1, -1):
+                output.append(matrix[bottom][i])
+            bottom -= 1
+
+            for i in range(bottom, top-1, -1):
+                output.append(matrix[i][left])
+            left += 1
         return output
