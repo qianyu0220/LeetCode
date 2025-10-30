3a4973614b4a951c5c2ae4c692b93ceb1d517466
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-29 18:41:31 -0700

    LC-majority-element [Runtime: 7 ms; Memory: 19.3 MB]

--- sorting/majority-element.py
+++ sorting/majority-element.py
@@ -3,6 +3,6 @@ class Solution:
         count = {}
         n = len(nums)
         for num in nums:
-            count[num] = count.get(num, 0) + 1
+            count[num] = count.get(num, 0)+1
             if count[num] > n // 2:
                 return num
