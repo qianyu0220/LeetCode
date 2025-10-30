8fa79682ed99a6da19e6b8d7d1cb7b78c724e540
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-30 11:00:10 -0700

    LC-coin-change [Runtime: 717 ms; Memory: 18 MB]

--- array/coin-change.py
+++ array/coin-change.py
@@ -1,9 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [float("inf")] * (amount + 1)
+        dp = [float("inf")] * (amount+1)
+        output = 0
         dp[0] = 0
         for i in range(1, amount+1):
             for coin in coins:
-                if i>=coin:
+                if i >= coin:
                     dp[i] = min(dp[i], dp[i-coin] + 1)
-        return dp[-1] if dp[-1] != float("inf") else -1
+        return dp[-1] if dp[-1] != float("inf") else -1 
