716cc425d7c2465fc3d71e36fa2d942d4cb2e385
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-10-30 14:24:11 -0700

    LC-word-search [Runtime: 3097 ms; Memory: 17.8 MB]

--- string/word-search.py
+++ string/word-search.py
@@ -1,19 +1,19 @@
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        rows = len(board)
-        cols = len(board[0])
-        def dfs(i, j, index):
-            if index == len(word):
+        m = len(board)
+        n = len(board[0])
+        def dfs(i, j, k):
+            if k == len(word):
                 return True
-            if i<0 or j<0 or i>=rows or j>=cols or board[i][j] != word[index]:
+            if i<0 or j<0 or i>=m or j>=n or board[i][j]!=word[k]:
                 return False
             tmp = board[i][j]
             board[i][j] = "#"
-            found = (dfs(i+1, j, index+1) or dfs(i-1, j, index+1) or dfs(i, j+1, index+1) or dfs(i, j-1, index+1))
+            found = (dfs(i+1,j,k+1) or dfs(i-1,j,k+1) or dfs(i,j+1,k+1) or dfs(i,j-1,k+1))
             board[i][j] = tmp
             return found
-        for i in range(rows):
-            for j in range(cols):
-                if dfs(i,j,0):
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0] and dfs(i,j,0):
                     return True
         return False
