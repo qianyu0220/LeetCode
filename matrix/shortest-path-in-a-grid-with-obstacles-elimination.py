7660a5611616eed4d1e69f6dd54f8e8d919da92c
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-03 10:38:38 -0800

    LC-shortest-path-in-a-grid-with-obstacles-elimination [Runtime: 55 ms; Memory: 17.9 MB]

--- matrix/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ matrix/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,32 +1,16 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        # m, n = len(grid), len(grid[0])
-        # directions = [(1,0), (-1, 0), (0,1), (0,-1)]
-        # visited = [[-1] * n for _ in range(m)]
-        # queue = deque([(0, 0, k, 0)])
-        # while queue:
-        #     x, y, remain, steps = queue.popleft()
-        #     if x == m - 1 and y == n - 1:
-        #         return steps
-        #     for dx, dy in directions:
-        #         nx, ny = x+dx, y+dy
-        #         if 0 <=nx <m and 0 <= ny < n:
-        #             nk = remain - grid[nx][ny]
-        #             if nk >= 0 and nk > visited[nx][ny]:
-        #                 visited[nx][ny] = nk
-        #                 queue.append((nx, ny, nk, steps+1))
-        # return -1
         m, n = len(grid), len(grid[0])
-        directions = [(1,0), (-1,0), (0,1), (0,-1)]
         visited = [[-1] * n for _ in range(m)]
         queue = deque([(0, 0, k, 0)])
+        directions = [(0,1), (0,-1), (1,0), (-1,0)]
         while queue:
             x, y, remain, steps = queue.popleft()
             if x == m-1 and y == n-1:
                 return steps
             for dx, dy in directions:
                 nx, ny = x+dx, y+dy
-                if 0 <= nx < m and 0 <= ny < n:
+                if 0<=nx<m and 0<=ny<n:
                     nk = remain - grid[nx][ny]
                     if nk >= 0 and nk > visited[nx][ny]:
                         visited[nx][ny] = nk
