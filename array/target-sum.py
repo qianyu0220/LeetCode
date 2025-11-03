2dfdb2fad14d47a7ab9316454fa5d1561c6b5f08
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-03 10:12:48 -0800

    LC-target-sum [Runtime: 20 ms; Memory: 17.9 MB]

--- array/target-sum.py
+++ array/target-sum.py
@@ -1,12 +1,12 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         total = sum(nums)
-        P = (total+target) // 2
-        if (total+target) % 2 == 1 or abs(target) > total:
+        if (target+total) % 2 == 1 or abs(target) > total:
             return 0
-        dp = [0] * (P + 1)
+        P = (total + target) // 2
+        dp = [0] * (P+1)
         dp[0] = 1
         for num in nums:
             for i in range(P, num-1, -1):
-                dp[i] += dp[i-num] 
+                dp[i] += dp[i-num]
         return dp[P]
