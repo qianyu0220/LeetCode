f76dbdf09798f830dbb1e52b0b3ab654311f18de
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-03 13:46:52 -0800

    LC-search-suggestions-system [Runtime: 7 ms; Memory: 20.5 MB]

--- binary-search/search-suggestions-system.py
+++ binary-search/search-suggestions-system.py
@@ -1,5 +1,17 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
+        # products.sort()
+        # result = []
+        # prefix = ""
+        # for char in searchWord:
+        #     prefix += char
+        #     i = bisect.bisect_left(products, prefix)
+        #     suggestions = []
+        #     for j in range(i, min(i+3, len(products))):
+        #         if products[j].startswith(prefix):
+        #             suggestions.append(products[j])
+        #     result.append(suggestions)
+        # return result
         products.sort()
         result = []
         prefix = ""
