166e19bec06f388c122522670edb4657a0f21cee
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-03 14:55:07 -0800

    LC-subrectangle-queries [Runtime: 124 ms; Memory: 19.7 MB]

--- matrix/subrectangle-queries.py
+++ matrix/subrectangle-queries.py
@@ -2,13 +2,13 @@ class SubrectangleQueries:
 
     def __init__(self, rectangle: List[List[int]]):
         self.rectangle = rectangle
-        self.updates = []
+        self.update = []
 
     def updateSubrectangle(self, row1: int, col1: int, row2: int, col2: int, newValue: int) -> None:
-        self.updates.append((row1, col1, row2, col2, newValue))
+        self.update.append((row1, col1, row2, col2, newValue))
 
     def getValue(self, row: int, col: int) -> int:
-        for row1, col1, row2, col2, newValue in reversed(self.updates):
+        for row1, col1, row2, col2, newValue in reversed(self.update):
             if row1<=row<=row2 and col1<=col<=col2:
                 return newValue
         return self.rectangle[row][col]
