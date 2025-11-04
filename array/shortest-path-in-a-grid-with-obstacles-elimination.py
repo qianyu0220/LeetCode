51afae552b19742d072a41443f23ff4d738fcf96
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-04 12:12:51 -0800

    LC-shortest-path-in-a-grid-with-obstacles-elimination [Runtime: 47 ms; Memory: 18 MB]

--- array/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ array/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,22 +1,18 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
         m, n = len(grid), len(grid[0])
-        output = 0
-        queue = deque([(0,0,k,0)])
         directions = [(1,0), (-1,0), (0,1), (0,-1)]
         visited = [[-1] * n for _ in range(m)]
-
+        queue = deque([(0, 0, k, 0)])
         while queue:
             x, y, remain, steps = queue.popleft()
             if x == m-1 and y == n-1:
                 return steps
             for dx, dy in directions:
-                nx = x + dx
-                ny = y + dy
+                nx, ny = x+dx, y+dy
                 if 0<=nx<m and 0<=ny<n:
                     nk = remain - grid[nx][ny]
-                    if nk >= 0 and nk > visited[nx][ny]:
+                    if nk>=0 and nk > visited[nx][ny]:
                         visited[nx][ny] = nk
                         queue.append((nx, ny, nk, steps+1))
         return -1
-
