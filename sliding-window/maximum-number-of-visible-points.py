775d1bda2e4d46cb3f9a1dbb8da828afda08fac8
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-03 19:45:41 -0800

    LC-maximum-number-of-visible-points [Runtime: N/A; Memory: N/A]

--- sliding-window/maximum-number-of-visible-points.py
+++ sliding-window/maximum-number-of-visible-points.py
@@ -1,9 +1,8 @@
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
         x0, y0 = location
-        same = 0
         angles = []
-
+        same = 0
         for x, y in points:
             dx, dy = x - x0, y - y0
             if dx == 0 and dy == 0:
@@ -12,12 +11,10 @@ class Solution:
                 angles.append(math.degrees(math.atan2(dy, dx)))
         angles.sort()
         n = len(angles)
-        angles += [a  + 360 for a in angles]
-
         max_count = 0
         j = 0
         for i in range(n):
             while j < len(angles) and angles[j] - angles[i] <= angle:
                 j += 1
-            max_count = max(max_count, j-i)
+            max_count = max(max_count, j - i)
         return same + max_count
