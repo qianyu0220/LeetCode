5ab33f3d0b0efcf0d17850968453b8f6d925fed2
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-04 14:41:15 -0800

    LC-merge-intervals [Runtime: 7 ms; Memory: 20.9 MB]

--- sorting/merge-intervals.py
+++ sorting/merge-intervals.py
@@ -8,10 +8,10 @@ class Solution:
         #     else:
         #         merged[-1][1] = max(merged[-1][1], interval[1])
         # return merged
-        intervals.sort(key=lambda x:x[0])
+        intervals.sort(key=lambda x: x[0])
         merged = []
         for interval in intervals:
-            if not merged or interval[0] > merged[-1][1]:
+            if not merged or merged[-1][1] < interval[0]:
                 merged.append(interval)
             else:
                 merged[-1][1] = max(merged[-1][1], interval[1])
