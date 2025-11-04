ee56c3c51db4c5512a3d490bbe7219f6ab8f54c8
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-04 14:29:32 -0800

    LC-find-a-peak-element-ii [Runtime: 0 ms; Memory: 46 MB]

--- array/find-a-peak-element-ii.py
+++ array/find-a-peak-element-ii.py
@@ -14,7 +14,7 @@ class Solution:
             right_val = mat[max_row][mid+1] if mid+1 < n else -1
             if mat[max_row][mid]>left_val and mat[max_row][mid] > right_val:
                 return [max_row, mid]
-            elif mat[max_row][mid] < left_max:
+            elif mat[max_row][mid] < left_val:
                 right = mid - 1
             else:
                 left = mid + 1
