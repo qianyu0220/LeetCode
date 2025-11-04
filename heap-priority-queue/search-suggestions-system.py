b2d85ef03d5af755e9c143aea3cd5e4a62b5945e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-04 10:23:14 -0800

    LC-search-suggestions-system [Runtime: 4 ms; Memory: 20.6 MB]

--- heap-priority-queue/search-suggestions-system.py
+++ heap-priority-queue/search-suggestions-system.py
@@ -1,20 +1,8 @@
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        # products.sort()
-        # result = []
-        # prefix = ""
-        # for char in searchWord:
-        #     prefix += char
-        #     suggestions = []
-        #     i = bisect.bisect_left(products, prefix)
-        #     for j in range(i, min(i+3, len(products))):
-        #         if products[j].startswith(prefix):
-        #             suggestions.append(products[j])
-        #     result.append(suggestions)
-        # return result
         products.sort()
-        result = []
         prefix = ""
+        output = []
         for char in searchWord:
             prefix += char
             suggestions = []
@@ -22,5 +10,5 @@ class Solution:
             for j in range(i, min(i+3, len(products))):
                 if products[j].startswith(prefix):
                     suggestions.append(products[j])
-            result.append(suggestions)
-        return result
+            output.append(suggestions)
+        return output
