a1cc6b15796ce5287cb68d7fb0be418e284dbf91
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-03 18:20:31 -0800

    LC-moving-average-from-data-stream [Runtime: 4 ms; Memory: 21.1 MB]

---
+++ array/moving-average-from-data-stream.py
@@ -0,0 +1,19 @@
+class MovingAverage:
+
+    def __init__(self, size: int):
+        self.size = size
+        self.window_sum = 0
+        self.queue = deque()
+
+    def next(self, val: int) -> float:
+        if self.size == len(self.queue):
+            self.window_sum -= self.queue.popleft() 
+        self.queue.append(val)
+        self.window_sum += val
+        return self.window_sum/len(self.queue)
+        
+
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
