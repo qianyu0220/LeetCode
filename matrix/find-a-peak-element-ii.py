cc6b66f18982a62d07d73b83eab4f5431a4f9a4b
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-04 14:29:20 -0800

    LC-find-a-peak-element-ii [Runtime: N/A; Memory: N/A]

--- matrix/find-a-peak-element-ii.py
+++ matrix/find-a-peak-element-ii.py
@@ -2,22 +2,19 @@ class Solution:
     def findPeakGrid(self, mat: List[List[int]]) -> List[int]:
         m, n = len(mat), len(mat[0])
         left, right = 0, n-1
-        output = []
         while left <= right:
             mid = (left + right) // 2
+            cur_max = 0
             max_row = 0
-            max_cur = 0
             for i in range(m):
-                if mat[i][mid] > max_cur:
-                    max_cur = mat[i][mid]
+                if mat[i][mid] > cur_max:
                     max_row = i
-            left_val = mat[max_row][mid-1] if mid-1>=0 else -1
+                    cur_max = mat[i][mid]
+            left_val = mat[max_row][mid-1] if mid-1 >= 0 else -1
             right_val = mat[max_row][mid+1] if mid+1 < n else -1
-
-            if mat[max_row][mid] > left_val and mat[max_row][mid] >right_val:
+            if mat[max_row][mid]>left_val and mat[max_row][mid] > right_val:
                 return [max_row, mid]
-            elif mat[max_row][mid] < left_val:
+            elif mat[max_row][mid] < left_max:
                 right = mid - 1
             else:
                 left = mid + 1
-
