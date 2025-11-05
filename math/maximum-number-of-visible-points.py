a63ddab985dbf872e50f50f6850661d218658526
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-04 19:17:27 -0800

    LC-maximum-number-of-visible-points [Runtime: 235 ms; Memory: 45.9 MB]

--- math/maximum-number-of-visible-points.py
+++ math/maximum-number-of-visible-points.py
@@ -1,10 +1,11 @@
 class Solution:
     def visiblePoints(self, points: List[List[int]], angle: int, location: List[int]) -> int:
         x0, y0 = location
-        angles = []
         same = 0
+        angles = []
         for x, y in points:
-            dx, dy = x - x0, y - y0
+            dx = x - x0
+            dy = y - y0
             if dx == 0 and dy == 0:
                 same += 1
             else:
@@ -19,6 +20,4 @@ class Solution:
             while j < len(angles) and angles[j] - angles[i] <= angle:
                 j += 1
             max_count = max(max_count, j - i)
-        return same + max_count
-
-
+        return max_count + same
