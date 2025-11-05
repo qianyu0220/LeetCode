5d8ecf39b0e6a2dc8bdaabc50426fc3f86b98ce5
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-04 19:47:29 -0800

    LC-interval-list-intersections [Runtime: 5 ms; Memory: 18.5 MB]

--- line-sweep/interval-list-intersections.py
+++ line-sweep/interval-list-intersections.py
@@ -1,5 +1,19 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
+        # output = []
+        # m = len(firstList)
+        # n = len(secondList)
+        # i = j = 0
+        # while i < m and j < n:
+        #     start = max(firstList[i][0], secondList[j][0])
+        #     end = min(firstList[i][1], secondList[j][1])
+        #     if start <= end:
+        #         output.append([start, end])
+        #     if firstList[i][1] < secondList[j][1]:
+        #         i += 1
+        #     else:
+        #         j += 1
+        # return output
         output = []
         m = len(firstList)
         n = len(secondList)
