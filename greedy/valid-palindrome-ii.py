84f47c7fb736339ebe74b75f1bc5bb659dea1ab2
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-09 15:39:06 -0800

    LC-valid-palindrome-ii [Runtime: 61 ms; Memory: 17.8 MB]

--- greedy/valid-palindrome-ii.py
+++ greedy/valid-palindrome-ii.py
@@ -5,7 +5,7 @@ class Solution:
                 if s[left] != s[right]:
                     return False
                 left += 1
-                right += 1
+                right -= 1
             return True
         left, right = 0, len(s) - 1
         while left < right:
