38820cdb434387d9d19d51d1ee980215d4b0c660
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-09 15:38:08 -0800

    LC-valid-palindrome-ii [Runtime: N/A; Memory: N/A]

--- greedy/valid-palindrome-ii.py
+++ greedy/valid-palindrome-ii.py
@@ -1,20 +1,16 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        
-
         def is_palindrome(left, right):
             while left < right:
                 if s[left] != s[right]:
                     return False
                 left += 1
-                right -= 1
+                right += 1
             return True
-        l = 0
-        r = len(s) - 1
-        while l < r:
-            if s[l] != s[r]:
-                return is_palindrome(l+1, r) or is_palindrome(l, r-1)
-            l += 1
-            r -= 1
-
+        left, right = 0, len(s) - 1
+        while left < right:
+            if s[left] != s[right]:
+                return is_palindrome(left+1, right) or is_palindrome(left, right - 1)
+            left += 1
+            right -= 1
         return True
