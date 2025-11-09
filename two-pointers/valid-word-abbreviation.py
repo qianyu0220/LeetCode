8ec4effa974830911af239bf533b8d0776badbb3
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-09 14:44:24 -0800

    LC-valid-word-abbreviation [Runtime: 0 ms; Memory: 17.7 MB]

---
+++ two-pointers/valid-word-abbreviation.py
@@ -0,0 +1,24 @@
+class Solution:
+    def validWordAbbreviation(self, word: str, abbr: str) -> bool:
+        i = j = 0
+        m = len(word)
+        n = len(abbr)
+        while i < m and j < n:
+            if abbr[j].isalpha():
+                if abbr[j] == word[i]:
+                    i += 1
+                    j += 1
+                else:
+                    return False
+            elif abbr[j].isdigit():
+                if abbr[j] == "0":
+                    return False
+                num_str = ""
+                while j < n and abbr[j].isdigit():
+                    num_str += abbr[j]
+                    j += 1
+                i += int(num_str)
+            else:
+                return False           
+        return i == m and j == n
+           
