1d504bb5b545da209cf2e538438327367fb0ec72
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-13 16:43:55 -0800

    LC-single-number [Runtime: 0 ms; Memory: 19.5 MB]

--- array/single-number.py
+++ array/single-number.py
@@ -1,5 +1,9 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        # output = 0
+        # for num in nums:
+        #     output = output ^ num
+        # return output
         output = 0
         for num in nums:
             output = output ^ num
