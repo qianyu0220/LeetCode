68c5695147c6ca39b9eed52457ae3900a0f56758
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-11-13 16:11:21 -0800

    LC-single-element-in-a-sorted-array [Runtime: 0 ms; Memory: 24.8 MB]

--- binary-search/single-element-in-a-sorted-array.py
+++ binary-search/single-element-in-a-sorted-array.py
@@ -1,8 +1,13 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         n = len(nums)
-        if n == 1:
-            return nums[0]
-        for i in range(1, n-1):
-            if nums[i] != nums[i-1] and nums[i] != nums[i+1]:
-                return nums[i]
+        left, right = 0, n - 1
+        while left < right:
+            mid = (left + right) // 2
+            if mid % 2 == 1:
+                mid -= 1
+            if nums[mid] != nums[mid + 1]:
+                right = mid
+            else:
+                left = mid + 2
+        return nums[left]
