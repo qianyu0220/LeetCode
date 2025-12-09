8a092b0f7cc8c31e2359c7aaf11b834eb80c5d93
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-08 21:32:43 -0500

    LC-flood-fill [Runtime: 0 ms; Memory: 18.2 MB]

--- array/flood-fill.py
+++ array/flood-fill.py
@@ -1,16 +1,16 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        start_color = image[sr][sc]
         m, n = len(image), len(image[0])
+        start_color = image[sr][sc]
         def backtrack(i, j, grid):
-            if i<0 or i>=m or j<0 or j>=n:
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j]==color:
                 return
-            if start_color != grid[i][j] or grid[i][j] == color:
+            if start_color != grid[i][j]:
                 return
             grid[i][j] = color
-            backtrack(i-1, j, grid)
             backtrack(i+1, j, grid)
-            backtrack(i, j-1, grid)
+            backtrack(i-1, j, grid)
             backtrack(i, j+1, grid)
+            backtrack(i, j-1, grid)
         backtrack(sr, sc, image)
         return image
