c7e5f78af40b8697f094c55803bacc6520fd19e8
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-08 21:09:49 -0500

    LC-maximum-subarray [Runtime: 63 ms; Memory: 32.9 MB]

--- divide-and-conquer/maximum-subarray.py
+++ divide-and-conquer/maximum-subarray.py
@@ -1,11 +1,11 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        output = nums[0]
-        cur_sum = nums[0]
         n = len(nums)
-        for i in range(1, n):
-            if cur_sum + nums[i] < nums[i]:
+        cur_sum = nums[0]
+        output = nums[0]
+        for right in range(1, n):
+            if cur_sum + nums[right] < nums[right]:
                 cur_sum = 0
-            cur_sum += nums[i]
+            cur_sum += nums[right]
             output = max(output, cur_sum)
         return output
