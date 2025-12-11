6d8791efd426180940036442fa62492a674b9f48
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-11 14:15:11 -0500

    LC-remove-all-adjacent-duplicates-in-string [Runtime: 15 ms; Memory: 18.7 MB]

--- string/remove-all-adjacent-duplicates-in-string.py
+++ string/remove-all-adjacent-duplicates-in-string.py
@@ -1,13 +1,7 @@
 class Solution:
     def removeDuplicates(self, s: str) -> str:
-        # stack = []
-        # for char in s:
-        #     if stack and stack[-1] == char:
-        #         stack.pop()
-        #     else:
-        #         stack.append(char)
-        # return "".join(stack)
         stack = []
+        n = len(s)
         for char in s:
             if stack and stack[-1] == char:
                 stack.pop()
