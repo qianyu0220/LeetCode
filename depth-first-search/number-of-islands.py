6ab46efaf5c9abd892eece69b769256e065c456d
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-10 22:35:32 -0500

    LC-number-of-islands [Runtime: 233 ms; Memory: 20.3 MB]

--- depth-first-search/number-of-islands.py
+++ depth-first-search/number-of-islands.py
@@ -4,13 +4,13 @@ class Solution:
         n = len(grid[0])
         output = 0
         def dfs(i, j):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j] != "1":
-                return 
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j]!="1":
+                return
             grid[i][j] = "0"
-            dfs(i-1, j)
             dfs(i+1, j)
-            dfs(i, j-1)
+            dfs(i-1, j)
             dfs(i, j+1)
+            dfs(i, j-1)
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
