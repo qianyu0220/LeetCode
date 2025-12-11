7c50815c50c2bfdc4b612d1a86e2f693838d4a0f
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-11 14:14:10 -0500

    LC-two-sum [Runtime: 0 ms; Memory: 18.9 MB]

--- hash-table/two-sum.py
+++ hash-table/two-sum.py
@@ -2,8 +2,8 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
         n = len(nums)
-        for i in range(n):
+        for i, num in enumerate(nums):
             complement = target - nums[i]
             if complement in hashmap:
                 return [i, hashmap[complement]]
-            hashmap[nums[i]] = i
+            hashmap[num] = i
