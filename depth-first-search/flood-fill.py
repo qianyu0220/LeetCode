1e1995ef688d2482f15661fad0da10a7528cffa6
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-10 23:10:44 -0500

    LC-flood-fill [Runtime: 0 ms; Memory: 18 MB]

--- depth-first-search/flood-fill.py
+++ depth-first-search/flood-fill.py
@@ -1,16 +1,17 @@
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        m, n = len(image), len(image[0])
         start_color = image[sr][sc]
-        def backtrack(i, j, grid):
-            if i<0 or i>=m or j<0 or j>=n or grid[i][j]==color:
+        m, n = len(image), len(image[0])
+        def backtracking(i, j, grid):
+            if i<0 or i>=m or j<0 or j>=n or grid[i][j] == color:
                 return
-            if start_color != grid[i][j]:
-                return
+            if grid[i][j] != start_color:
+                return 
             grid[i][j] = color
-            backtrack(i+1, j, grid)
-            backtrack(i-1, j, grid)
-            backtrack(i, j+1, grid)
-            backtrack(i, j-1, grid)
-        backtrack(sr, sc, image)
+            backtracking(i+1, j, grid)
+            backtracking(i-1, j, grid)
+            backtracking(i, j+1, grid)
+            backtracking(i, j-1, grid)
+        backtracking(sr, sc, image)
         return image
+
