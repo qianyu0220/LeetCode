183cc3ac05a658a96105322527489d871f157dcb
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-15 16:48:07 -0500

    LC-rectangle-area [Runtime: 8 ms; Memory: 17.9 MB]

--- math/rectangle-area.py
+++ math/rectangle-area.py
@@ -1,10 +1,9 @@
 class Solution:
     def computeArea(self, ax1: int, ay1: int, ax2: int, ay2: int, bx1: int, by1: int, bx2: int, by2: int) -> int:
-        x = [ax1, ax2, bx1, bx2]
-        y = [ay1, ay2, by1, by2]
-        x.sort()
-        y.sort()
         area1 = (ax2 - ax1) * (ay2 - ay1)
         area2 = (bx2 - bx1) * (by2 - by1)
 
-        return (area1 + area2) - (x[2] - x[1]) * (y[2] - y[1])
+        overlap_width = max(0, min(ax2, bx2) - max(ax1, bx1))
+        overlap_height = max(0, min(ay2, by2) - max(by1, ay1))
+
+        return area1 + area2 - overlap_width * overlap_height
