0fe7cd3168c739b23bb485015edbdf8d97821cce
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-15 18:04:33 -0500

    LC-maximum-average-subarray-i [Runtime: N/A; Memory: N/A]

---
+++ array/maximum-average-subarray-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        cur_sum = sum(nums[:k])
+        output = cur_sum / k
+        n = len(nums)
+
+        for i in range(k, n):
+            cur_sum = cur_sum - nums[i-k] + nums[i]
+            if cur_sum / 4 > output:
+                output = cur_sum / 4
+        return output
