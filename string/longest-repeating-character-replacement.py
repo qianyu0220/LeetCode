84bee4ffccfc5e747471ee085b2f270123ca2694
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-14 19:41:44 -0500

    LC-longest-repeating-character-replacement [Runtime: N/A; Memory: N/A]

--- string/longest-repeating-character-replacement.py
+++ string/longest-repeating-character-replacement.py
@@ -1,13 +1,13 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
         count = {}
-        left = 0
-        max_freq = 0
+        n = len(s)
         output = 0
-        for right in range(len(s)):
+        cur_freq = 0
+        left = 0
+        for right in range(n):
             count[s[right]] = count.get(s[right], 0) + 1
-            max_freq = max(max_freq, count[s[right]])
-            if right - left + 1 - max_freq > k:
+            while right - left + 1 - count[s[right]] > k:
                 count[s[left]] -= 1
                 left += 1
             output = max(output, right - left + 1)
