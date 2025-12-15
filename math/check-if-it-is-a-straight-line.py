8b34d9de685a6cba992115eccf7f7a392fde1399
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-15 18:20:56 -0500

    LC-check-if-it-is-a-straight-line [Runtime: 0 ms; Memory: 18.1 MB]

--- math/check-if-it-is-a-straight-line.py
+++ math/check-if-it-is-a-straight-line.py
@@ -1,9 +1,12 @@
 class Solution:
     def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
-        n = len(coordinates)
-        diff = (coordinates[1][1] - coordinates[0][1]) / (coordinates[1][0] - coordinates[0][0])
+        x0, y0 = coordinates[0]
+        x1, y1 = coordinates[1]
 
-        for i in range(1, n):
-            if (coordinates[i][1] - coordinates[i-1][1]) / (coordinates[i][0] - coordinates[i-1][0]) != diff:
+        dx = x1 - x0
+        dy = y1 - y0
+
+        for x, y in coordinates[2:]:
+            if dx * (y - y0) != dy * (x - x0):
                 return False
         return True
