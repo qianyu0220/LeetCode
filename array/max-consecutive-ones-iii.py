97e33c1dd021ad7f3459e342bd6a2349e59c1cde
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-16 13:49:46 -0500

    LC-max-consecutive-ones-iii [Runtime: 97 ms; Memory: 20.4 MB]

--- array/max-consecutive-ones-iii.py
+++ array/max-consecutive-ones-iii.py
@@ -1,13 +1,13 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        cur_sum = 0
         n = len(nums)
         output = 0
         left = 0
+        cur_sum = 0
         for right in range(n):
             cur_sum += nums[right]
-            while (right - left + 1) - cur_sum > k:
-                cur_sum -= nums[left]
+            while right - left + 1 - cur_sum > k:
+                cur_sum -= nums[left] 
                 left += 1
             output = max(output, right - left + 1)
         return output
