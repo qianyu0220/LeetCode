630fa9aa2d0d3dc2801f7975c25fcb5d39343668
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-16 14:25:36 -0500

    LC-subarray-sum-equals-k [Runtime: N/A; Memory: N/A]

--- prefix-sum/subarray-sum-equals-k.py
+++ prefix-sum/subarray-sum-equals-k.py
@@ -1,11 +1,15 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        cur = defaultdict(int)
+        n = len(nums)
         output = 0
-        pre = 0
-        cur[0] = 1
-        for x in nums:
-            pre += x
-            output += cur[pre - k]
-            cur[pre] += 1
+        left = 0
+        cur_sum = 0
+        for right in range(n):
+            cur_sum += nums[right]
+            while cur_sum > k:
+                
+                cur_sum -= nums[left] 
+                left += 1
+            if cur_sum == k:
+                output += 1
         return output
