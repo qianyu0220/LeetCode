66e498019be87ef4e644d3739f7471c8fd6e2799
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-16 21:13:14 -0500

    LC-lru-cache [Runtime: 96 ms; Memory: 77.9 MB]

---
+++ design/lru-cache.py
@@ -0,0 +1,57 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.cache = {}
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+    
+    def add_to_head(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key not in self.cache:
+            return -1
+        node = self.cache[key]
+        self.remove(node)
+        self.add_to_head(node)
+        return node.val      
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            node = self.cache[key]
+            node.val = value
+            self.remove(node)
+            self.add_to_head(node)
+        else:
+            if len(self.cache) == self.cap:
+                lru = self.tail.prev
+                self.remove(lru)
+                del self.cache[lru.key]
+            new_node = Node(key, value)
+            self.cache[key] = new_node
+            self.add_to_head(new_node)
+        
+
+
+# Your LRUCache object will be instantiated and called as such:
+# obj = LRUCache(capacity)
+# param_1 = obj.get(key)
+# obj.put(key,value)
