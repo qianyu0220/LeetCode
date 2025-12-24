22a15fd391ad2b4309282efd1b25cf3424f778ff
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2025-12-23 20:13:25 -0500

    LC-number-of-islands [Runtime: 225 ms; Memory: 19.7 MB]

--- union-find/number-of-islands.py
+++ union-find/number-of-islands.py
@@ -3,7 +3,6 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
         output = 0
-
         def dfs(i, j):
             if i<0 or i>=m or j<0 or j>=n or grid[i][j] != "1":
                 return 
@@ -12,7 +11,6 @@ class Solution:
             dfs(i-1, j)
             dfs(i, j+1)
             dfs(i, j-1)
-        
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
