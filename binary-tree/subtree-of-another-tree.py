ce923b6e58fef7b50fd8a70723771cf46de14710
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-01-04 21:26:47 -0500

    LC-subtree-of-another-tree [Runtime: 39 ms; Memory: 17.4 MB]

---
+++ binary-tree/subtree-of-another-tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        def isSameTree(q, p):
+            if not p and not q:
+                return True
+            if not p or not q:
+                return False
+            if p.val != q.val:
+                return False
+            return isSameTree(p.left, q.left) and isSameTree(p.right, q.right)
+        def helper(p, q):
+            if isSameTree(p, q):
+                return True
+            if not p:
+                return False
+            return helper(p.left, q) or helper(p.right, q)
+        return helper(root, subRoot)
