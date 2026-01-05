c07b4bd480877f67011465124ff3f33e95dae036
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-01-04 20:21:22 -0500

    LC-number-of-connected-components-in-an-undirected-graph [Runtime: N/A; Memory: N/A]

--- union-find/number-of-connected-components-in-an-undirected-graph.py
+++ union-find/number-of-connected-components-in-an-undirected-graph.py
@@ -2,6 +2,7 @@ class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
         m = len(edges)
         output = 1
+        edges.sort(key=lambda x: x[0])
         for i in range(m):
             if i>0 and edges[i][0] != edges[i-1][1]:
                 output += 1
