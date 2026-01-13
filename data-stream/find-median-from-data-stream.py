8d39e90ef82438f4b3f6b4d577c7bcc27ced29c0
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-01-13 15:57:23 -0500

    LC-find-median-from-data-stream [Runtime: 188 ms; Memory: 42.2 MB]

--- data-stream/find-median-from-data-stream.py
+++ data-stream/find-median-from-data-stream.py
@@ -9,12 +9,12 @@ class MedianFinder:
         heapq.heappush(self.large, -heapq.heappop(self.small))
         if len(self.small) < len(self.large):
             heapq.heappush(self.small, -heapq.heappop(self.large))
+        
 
     def findMedian(self) -> float:
         if len(self.small) > len(self.large):
             return float(-self.small[0])
         return (-self.small[0] + self.large[0]) / 2.0
-        
 
 
 # Your MedianFinder object will be instantiated and called as such:
