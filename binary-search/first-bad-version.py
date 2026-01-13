94298e78fcd3a4815c5eec1cdd23ebc28292105f
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-01-13 17:21:49 -0500

    LC-first-bad-version [Runtime: N/A; Memory: N/A]

---
+++ binary-search/first-bad-version.py
@@ -0,0 +1,10 @@
+# The isBadVersion API is already defined for you.
+# def isBadVersion(version: int) -> bool:
+
+class Solution:
+    def firstBadVersion(self, n: int) -> int:
+        output = float("inf")
+        for i in range(n):
+            if isBadVersion(i):
+                output = min(output, i)
+        return output
