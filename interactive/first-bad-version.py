35d13da656b584566dfd86c0a9c65a5eaa8873d5
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-01-13 23:05:09 -0500

    LC-first-bad-version [Runtime: 45 ms; Memory: 19.5 MB]

--- interactive/first-bad-version.py
+++ interactive/first-bad-version.py
@@ -3,12 +3,12 @@
 
 class Solution:
     def firstBadVersion(self, n: int) -> int:
-        first, last = 1, n
-        while first < last:
-            mid = (first + last) // 2
-
+        left = 1 
+        right = n
+        while left < right:
+            mid = (left + right) // 2
             if isBadVersion(mid):
-                last = mid
+                right = mid
             else:
-                first = mid + 1
-        return first
+                left = mid + 1
+        return left
