590512dc16544afab30933fe0735d7d25aa3418e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-20 20:56:41 -0400

    LC-contains-duplicate [Runtime: 6 ms; Memory: 32.2 MB]

--- hash-table/contains-duplicate.py
+++ hash-table/contains-duplicate.py
@@ -1,7 +1,6 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums_set = set()
-        n = len(nums)
         for num in nums:
             if num in nums_set:
                 return True
