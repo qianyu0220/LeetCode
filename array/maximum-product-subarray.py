166f187f60e17f24d402c6e485db2b2b882a9253
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-21 23:50:27 -0400

    LC-maximum-product-subarray [Runtime: 5 ms; Memory: 19.6 MB]

--- array/maximum-product-subarray.py
+++ array/maximum-product-subarray.py
@@ -1,8 +1,8 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        output = nums[0]
         cur_max = nums[0]
         cur_min = nums[0]
+        output = nums[0]
         for i in range(1, len(nums)):
             prev_max, prev_min = cur_max, cur_min
             cur_max = max(prev_max*nums[i], prev_min*nums[i], nums[i])
