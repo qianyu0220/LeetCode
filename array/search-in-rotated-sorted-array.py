b24a22e07db5c1338f598f8da9e8202a0c987c4f
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-24 01:44:22 -0400

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- array/search-in-rotated-sorted-array.py
+++ array/search-in-rotated-sorted-array.py
@@ -1,12 +1,12 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0 
-        right = len(nums) - 1
+        n = len(nums)
+        left, right = 0, n-1
         while left <= right:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
-            if nums[mid] >= nums[left]:
+            elif nums[mid] >= left:
                 if nums[left] <= target < nums[mid]:
                     right = mid - 1
                 else:
@@ -17,4 +17,3 @@ class Solution:
                 else:
                     right = mid - 1
         return -1
-
