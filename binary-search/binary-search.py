43e5a9fb18cad0bafcac31fa73d213064f46961f
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-24 01:11:03 -0400

    LC-binary-search [Runtime: N/A; Memory: N/A]

--- binary-search/binary-search.py
+++ binary-search/binary-search.py
@@ -1,13 +1,14 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums)
+        output = 0
+        n = len(nums)
+        left, right = 0, n-1
         while left < right:
             mid = (left + right) // 2
-            if nums[mid] < target:
-                left = mid + 1
-            elif nums[mid] == target:
+            if nums[mid] == target:
                 return mid
+            elif nums[mid] < target:
+                left = mid + 1
             else:
-                right = mid
+                right = mid - 1
         return -1
