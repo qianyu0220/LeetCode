224402598b4816948c459fb68921a29e2cb98db7
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-24 19:45:48 -0400

    LC-longest-repeating-character-replacement [Runtime: 75 ms; Memory: 19.7 MB]

--- hash-table/longest-repeating-character-replacement.py
+++ hash-table/longest-repeating-character-replacement.py
@@ -1,10 +1,23 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
+        # n = len(s)
+        # max_freq = 0
+        # left = 0
+        # output = 0
+        # count = {}
+        # for right in range(n):
+        #     count[s[right]] = count.get(s[right], 0) + 1
+        #     max_freq = max(max_freq, count[s[right]])
+        #     if right - left + 1 - max_freq > k:
+        #         count[s[left]] -= 1
+        #         left += 1
+        #     output = max(output, right - left + 1)
+        # return output
         n = len(s)
-        max_freq = 0
-        left = 0
-        output = 0
         count = {}
+        output = 0
+        left = 0
+        max_freq = 0
         for right in range(n):
             count[s[right]] = count.get(s[right], 0) + 1
             max_freq = max(max_freq, count[s[right]])
