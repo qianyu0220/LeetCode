24236a1c4e966868a14dd8d68bce94e278654ccb
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-26 18:31:10 -0400

    LC-symmetric-tree [Runtime: 0 ms; Memory: 19.5 MB]

--- tree/symmetric-tree.py
+++ tree/symmetric-tree.py
@@ -11,5 +11,7 @@ class Solution:
                 return True
             if not node1 or not node2:
                 return False
+            if node1.val != node2.val:
+                return False
             return check(node1.left, node2.right) and check(node1.right, node2.left)
         return check(root, root)
