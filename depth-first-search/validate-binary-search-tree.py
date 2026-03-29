9331f399f302d9652a54bec4e46f722f69d37c11
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-29 19:32:05 -0400

    LC-validate-binary-search-tree [Runtime: 0 ms; Memory: 20.9 MB]

--- depth-first-search/validate-binary-search-tree.py
+++ depth-first-search/validate-binary-search-tree.py
@@ -6,11 +6,10 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        
-        def isValid(node, low, high):
+        def dfs(node, low, high):
             if not node:
                 return True
             if not (low < node.val < high):
                 return False
-            return isValid(node.left, low, node.val) and isValid(node.right, node.val, high)
-        return isValid(root, float("-inf"), float("inf"))
+            return dfs(node.left, low, node.val) and dfs(node.right, node.val, high)
+        return dfs(root, float("-inf"), float("inf"))
