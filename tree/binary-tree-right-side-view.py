5a4d465d07c44006fe2892a369aff84c00679ba3
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-29 20:12:15 -0400

    LC-binary-tree-right-side-view [Runtime: 4 ms; Memory: 19.4 MB]

--- tree/binary-tree-right-side-view.py
+++ tree/binary-tree-right-side-view.py
@@ -6,22 +6,6 @@
 #         self.right = right
 class Solution:
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        # queue = deque([root])
-        # output = []
-        # if not root:
-        #     return []
-
-        # while queue:
-        #     n = len(queue)
-        #     for i in range(n):
-        #         node = queue.popleft()
-        #         if node.left:
-        #             queue.append(node.left)
-        #         if node.right:
-        #             queue.append(node.right)
-        #         if i == n-1:
-        #             output.append(node.val)
-        # return output
         output = []
         queue = deque([root])
         if not root:
