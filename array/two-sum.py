e1ccae5e0dd4e7f0bd1c38248d1206aa33a3c96e
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-31 16:34:32 -0400

    LC-two-sum [Runtime: 3 ms; Memory: 20.4 MB]

--- array/two-sum.py
+++ array/two-sum.py
@@ -1,9 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         hashmap = {}
+        output = []
         for index, i in enumerate(nums):
             complement = target - i
             if complement in hashmap:
                 return [index, hashmap[complement]]
-            else:
-                hashmap[i] = index
+            hashmap[i] = index
