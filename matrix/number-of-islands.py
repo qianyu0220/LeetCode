f13dc637409775b17368b697c6296964a6aacb1b
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-31 16:59:45 -0400

    LC-number-of-islands [Runtime: 238 ms; Memory: 21.7 MB]

--- matrix/number-of-islands.py
+++ matrix/number-of-islands.py
@@ -7,14 +7,14 @@ class Solution:
             if a<0 or a>=m or b<0 or b>=n or grid[a][b] != "1":
                 return
             grid[a][b] = "0"
-            dfs(a-1, b)
             dfs(a+1, b)
-            dfs(a, b-1)
+            dfs(a-1, b)
             dfs(a, b+1)
+            dfs(a, b-1)
         
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
-                    dfs(i, j)
+                    dfs(i,j)
                     output += 1
         return output
