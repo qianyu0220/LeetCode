d76166839b7b8029222e7cc70ddcab42ca4fc42b
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-03-31 16:45:40 -0400

    LC-container-with-most-water [Runtime: 46 ms; Memory: 29.5 MB]

--- array/container-with-most-water.py
+++ array/container-with-most-water.py
@@ -1,15 +1,14 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        cur_max = 0
+        output = 0
         n = len(height)
-        left = 0
-        right = n - 1
+        left, right = 0, n-1
         while left < right:
             if height[left] < height[right]:
-                water =  height[left] * (right - left)
-                left += 1
+                water = height[left] * (right-left)
+                left += 1              
             else:
-                water = height[right] * (right - left)
-                right -= 1
-            cur_max = max(water, cur_max)
-        return cur_max
+                water = height[right] * (right-left)
+                right -= 1               
+            output = max(output, water)
+        return output 
