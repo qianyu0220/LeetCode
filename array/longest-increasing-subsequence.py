429eba0772fdd1561189329d01ca3b5b1f5b53a1
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-16 00:37:55 -0400

    LC-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

--- array/longest-increasing-subsequence.py
+++ array/longest-increasing-subsequence.py
@@ -10,7 +10,7 @@ class Solution:
         n = len(nums)
         dp = [1] * n
         output = 1
-        for i in range(n):
+        for i in range(1, n):
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j]+1)
