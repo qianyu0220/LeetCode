c107a9f76fd32f6a8944e2b3d76587f77a584425
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-16 00:38:45 -0400

    LC-longest-increasing-subsequence [Runtime: 1250 ms; Memory: 19.4 MB]

--- dynamic-programming/longest-increasing-subsequence.py
+++ dynamic-programming/longest-increasing-subsequence.py
@@ -9,9 +9,8 @@ class Solution:
         # return max(dp)
         n = len(nums)
         dp = [1] * n
-        output = 1
         for i in range(1, n):
             for j in range(i):
                 if nums[i] > nums[j]:
                     dp[i] = max(dp[i], dp[j]+1)
-        return dp[-1]
+        return max(dp)
