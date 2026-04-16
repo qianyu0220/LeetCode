033773225e03e0acb97c44df82e7822a7903e2ea
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-16 00:33:49 -0400

    LC-longest-increasing-subsequence [Runtime: N/A; Memory: N/A]

--- dynamic-programming/longest-increasing-subsequence.py
+++ dynamic-programming/longest-increasing-subsequence.py
@@ -1,9 +1,17 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
+        # n = len(nums)
+        # dp = [1] * n
+        # for i in range(1, n):
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             dp[i] = max(dp[i], dp[j] + 1)
+        # return max(dp)
         n = len(nums)
         dp = [1] * n
-        for i in range(1, n):
+        output = 1
+        for i in range(n):
             for j in range(i):
                 if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j] + 1)
-        return max(dp)
+                    dp[i] = max(dp[i], dp[j]+1)
+        return dp[-1]
