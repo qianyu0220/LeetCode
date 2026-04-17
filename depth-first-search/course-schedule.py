bfec49e82ac1419d10f36c65e91481e6a9f70db8
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-17 14:24:10 -0400

    LC-course-schedule [Runtime: 7 ms; Memory: 20.3 MB]

--- depth-first-search/course-schedule.py
+++ depth-first-search/course-schedule.py
@@ -1,18 +1,32 @@
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # graph = [[] for _ in range(numCourses)]
+        # indegrees = [0] * numCourses
+        # for a, b in prerequisites:
+        #     graph[b].append(a)
+        #     indegrees[a] += 1
+        # queue = deque([i for i in range(numCourses) if indegrees[i] == 0])
+        # count = 0
+        # while queue:
+        #     cur = queue.popleft()
+        #     count += 1
+        #     for nxt in graph[cur]:
+        #         indegrees[nxt] -= 1
+        #         if indegrees[nxt] == 0:
+        #             queue.append(nxt)
+        # return True if numCourses == count else False
         graph = [[] for _ in range(numCourses)]
-        indegree = [0] * numCourses
+        indegrees = [0] * numCourses
         for a, b in prerequisites:
             graph[b].append(a)
-            indegree[a] += 1
-        queue = deque([i for i in range(numCourses) if indegree[i]==0])
+            indegrees[a] += 1
+        queue = deque([i for i in range(numCourses) if indegrees[i] == 0])
         count = 0
-
         while queue:
             cur = queue.popleft()
             count += 1
             for nxt in graph[cur]:
-                indegree[nxt] -= 1
-                if indegree[nxt] == 0:
+                indegrees[nxt] -= 1
+                if indegrees[nxt] == 0:
                     queue.append(nxt)
-        return True if count == numCourses else False
+        return True if numCourses == count else False
