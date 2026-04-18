8f885e9d1abacd55fb49d2e0e6099bd6135acd43
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-18 14:57:15 -0400

    LC-edit-distance [Runtime: 48 ms; Memory: 22.8 MB]

--- dynamic-programming/edit-distance.py
+++ dynamic-programming/edit-distance.py
@@ -14,4 +14,4 @@ class Solution:
                     dp[i][j] = dp[i-1][j-1]
                 else:
                     dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1])+1
-        return dp[m-1][n-1]
+        return dp[m][n]
