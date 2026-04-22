1f6bece4cc8805eda7225187d57ba6a3adabdc3c
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-22 00:40:57 -0400

    LC-number-of-connected-components-in-an-undirected-graph [Runtime: 0 ms; Memory: 20.5 MB]

--- graph/number-of-connected-components-in-an-undirected-graph.py
+++ graph/number-of-connected-components-in-an-undirected-graph.py
@@ -1,25 +1,5 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        # graph = [[] for _ in range(n)]
-        # for u, v in edges:
-        #     graph[u].append(v)
-        #     graph[v].append(u)
-        # visited = [False] * n
-        # def dfs(i):
-        #     stack = [i]
-        #     while stack:
-        #         node = stack.pop()
-        #         for nei in graph[node]:
-        #             if not visited[nei]:
-        #                 visited[nei] = True
-        #                 stack.append(nei)
-        # count = 0
-        # for i in range(n):
-        #     if not visited[i]:
-        #         visited[i] = True
-        #         dfs(i)
-        #         count += 1
-        # return count
         graph = [[] for _ in range(n)]
         for a, b in edges:
             graph[a].append(b)
@@ -40,4 +20,3 @@ class Solution:
                 dfs(i)
                 count += 1
         return count
-
