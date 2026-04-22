4adf8409320ecdaf3c8f413fd771ec3493cd3377
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-22 00:28:16 -0400

    LC-number-of-connected-components-in-an-undirected-graph [Runtime: 4 ms; Memory: 20.3 MB]

--- breadth-first-search/number-of-connected-components-in-an-undirected-graph.py
+++ breadth-first-search/number-of-connected-components-in-an-undirected-graph.py
@@ -1,9 +1,29 @@
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        # graph = [[] for _ in range(n)]
+        # for u, v in edges:
+        #     graph[u].append(v)
+        #     graph[v].append(u)
+        # visited = [False] * n
+        # def dfs(i):
+        #     stack = [i]
+        #     while stack:
+        #         node = stack.pop()
+        #         for nei in graph[node]:
+        #             if not visited[nei]:
+        #                 visited[nei] = True
+        #                 stack.append(nei)
+        # count = 0
+        # for i in range(n):
+        #     if not visited[i]:
+        #         visited[i] = True
+        #         dfs(i)
+        #         count += 1
+        # return count
         graph = [[] for _ in range(n)]
-        for u, v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
+        for a, b in edges:
+            graph[a].append(b)
+            graph[b].append(a)
         visited = [False] * n
         def dfs(i):
             stack = [i]
@@ -13,6 +33,7 @@ class Solution:
                     if not visited[nei]:
                         visited[nei] = True
                         stack.append(nei)
+
         count = 0
         for i in range(n):
             if not visited[i]:
@@ -20,3 +41,4 @@ class Solution:
                 dfs(i)
                 count += 1
         return count
+
