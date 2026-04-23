b53c01bb414978b047be6bb7f6b05e2add9a7824
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-23 13:32:36 -0400

    LC-k-closest-points-to-origin [Runtime: 77 ms; Memory: 25 MB]

--- geometry/k-closest-points-to-origin.py
+++ geometry/k-closest-points-to-origin.py
@@ -1,9 +1,9 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
         heap = []
+        output = []
         for i in points:
             heapq.heappush(heap, (i[0]**2 + i[1]**2, i))
-        output = []
         for i in range(k):
             output.append(heapq.heappop(heap)[1])
         return output
