07f8737d6877882aceefa89b2b97ed3298ab6920
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-25 21:23:21 -0400

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- array/longest-consecutive-sequence.py
+++ array/longest-consecutive-sequence.py
@@ -1,11 +1,21 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        nums = set(nums)
+        # nums = set(nums)
+        # output = 0
+        # for num in nums:
+        #     if num - 1 not in nums:
+        #         length = 0
+        #         while num + length in nums:
+        #             length += 1
+        #         output = max(output, length)
+        # return output
+        n = len(nums)
         output = 0
+        nums = set(nums)
         for num in nums:
             if num - 1 not in nums:
                 length = 0
                 while num + length in nums:
                     length += 1
-                output = max(output, length)
+            output = max(output, length)
         return output
