d30fc5be75dbc707becbd38ea511be513702150f
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-27 14:35:32 -0400

    LC-number-of-islands [Runtime: 239 ms; Memory: 21.4 MB]

--- depth-first-search/number-of-islands.py
+++ depth-first-search/number-of-islands.py
@@ -1,7 +1,6 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         m, n = len(grid), len(grid[0])
-        output = 0
         def dfs(i, j):
             if i<0 or i>=m or j<0 or j>=n or grid[i][j] != "1":
                 return 
@@ -10,6 +9,7 @@ class Solution:
             dfs(i-1, j)
             dfs(i, j+1)
             dfs(i, j-1)
+        output = 0
         for i in range(m):
             for j in range(n):
                 if grid[i][j] == "1":
