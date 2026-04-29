b3b4b686c03c1e8dd970bfc91f2580c63497fc52
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-29 18:20:59 -0400

    LC-search-insert-position [Runtime: N/A; Memory: N/A]

--- array/search-insert-position.py
+++ array/search-insert-position.py
@@ -1,13 +1,13 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        left = 0
-        right = len(nums) - 1
-        while left <= right:
+        n = len(nums)
+        left, right = 0, n-1
+        while left < right:
             mid = (left + right) // 2
             if nums[mid] == target:
                 return mid
             elif nums[mid] < target:
                 left = mid + 1
             else:
-                right = mid - 1
-        return left
+                right = mid -1
+        return right+1
