1df07529a444973044856c63cd9def9ba4b243fd
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-04-29 18:15:43 -0400

    LC-number-of-islands [Runtime: 238 ms; Memory: 21.7 MB]

--- breadth-first-search/number-of-islands.py
+++ breadth-first-search/number-of-islands.py
@@ -5,10 +5,10 @@ class Solution:
             if i<0 or i>=m or j<0 or j>=n or grid[i][j] != "1":
                 return 
             grid[i][j] = "0"
-            dfs(i+1, j)
             dfs(i-1, j)
-            dfs(i, j+1)
+            dfs(i+1, j)
             dfs(i, j-1)
+            dfs(i, j+1)
         output = 0
         for i in range(m):
             for j in range(n):
