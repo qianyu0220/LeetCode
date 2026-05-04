3b9d1735434b9747928b712ed38b12cd1fb938a1
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-05-04 19:38:42 -0400

    LC-set-matrix-zeroes [Runtime: 227 ms; Memory: 21 MB]

--- matrix/set-matrix-zeroes.py
+++ matrix/set-matrix-zeroes.py
@@ -3,18 +3,33 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        # rows = []
+        # cols = []
+        # m = len(matrix)
+        # n = len(matrix[0])
+        # for i in range(m):
+        #     for j in range(n):
+        #         if matrix[i][j] == 0:
+        #             rows.append(i)
+        #             cols.append(j)
+        # for row in rows:
+        #     for j in range(n):
+        #         matrix[row][j] = 0
+        # for i in range(m):
+        #     for col in cols:
+        #         matrix[i][col] = 0
+        m, n = len(matrix), len(matrix[0])
         rows = []
         cols = []
-        m = len(matrix)
-        n = len(matrix[0])
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
                     rows.append(i)
                     cols.append(j)
-        for row in rows:
+        for i in rows:
             for j in range(n):
-                matrix[row][j] = 0
+                matrix[i][j] = 0
+
         for i in range(m):
-            for col in cols:
-                matrix[i][col] = 0
+            for j in cols:
+                matrix[i][j] = 0
