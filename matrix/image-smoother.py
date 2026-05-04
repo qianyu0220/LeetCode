542a283651c6e1fcf7cbadc99d0b0cff1516b025
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-05-04 18:48:35 -0400

    LC-image-smoother [Runtime: 135 ms; Memory: 20.2 MB]

---
+++ matrix/image-smoother.py
@@ -0,0 +1,22 @@
+class Solution:
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        m, n = len(img), len(img[0])
+        output = [[0] * n for _ in range(m)]
+        directions = [(-1, -1), (-1, 0), (-1, 1),
+        (0, -1), (0, 0), (0, 1),
+        (1, -1), (1, 0), (1, 1)]
+
+        for i in range(m):
+            for j in range(n):
+                total = 0
+                count = 0
+
+                for di, dj in directions:
+                    x = i + di
+                    y = j + dj
+
+                    if 0 <= x < m and 0 <= y < n:
+                        total += img[x][y]
+                        count += 1
+                output[i][j] = total // count
+        return output
