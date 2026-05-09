cf8956f61c452df9c406d8f8d89e31917eaf420c
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-05-09 01:04:48 -0400

    LC-valid-parentheses [Runtime: 3 ms; Memory: 19.2 MB]

--- stack/valid-parentheses.py
+++ stack/valid-parentheses.py
@@ -17,4 +17,4 @@ class Solution:
             elif char in mapping.keys():
                 if not stack or stack.pop() != mapping[char]:
                     return False
-        return True
+        return not stack
