f09d1d6c13ad1728bc579f84af75ffcf277b1505
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-05-11 18:43:00 -0400

    LC-contains-duplicate [Runtime: N/A; Memory: N/A]

--- sorting/contains-duplicate.py
+++ sorting/contains-duplicate.py
@@ -1,10 +1,9 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
         nums_set = set()
-        n = len(nums)
-        for i in range(n):
-            if nums[i] in nums_set:
+        for num in nums:
+            if num in nums_set:
                 return True
             else:
-                nums_set.add(nums[i])
+                nums_set.add(num)
         return False
