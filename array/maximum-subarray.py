711de184f163be800b1183aba8b351089a42508b
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-05-11 19:46:59 -0400

    LC-maximum-subarray [Runtime: N/A; Memory: N/A]

--- array/maximum-subarray.py
+++ array/maximum-subarray.py
@@ -2,7 +2,7 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         n = len(nums)
         output = 0
-        cur_sum = 0
+        cur_sum = nums[0]
         for i in range(n):
             if cur_sum + nums[i] < nums[i]:
                 cur_sum = 0
