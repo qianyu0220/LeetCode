1b2f7ddad1f56d7c48a11779bfe8be398b6e51c1
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-05-15 17:41:33 -0400

    LC-binary-tree-level-order-traversal [Runtime: N/A; Memory: N/A]

--- binary-tree/binary-tree-level-order-traversal.py
+++ binary-tree/binary-tree-level-order-traversal.py
@@ -6,9 +6,25 @@
 #         self.right = right
 class Solution:
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        # if not root:
+        #     return []
+        # result = []
+        # queue = deque([root])
+        # while queue:
+        #     level = []
+        #     level_size = len(queue)
+        #     for _ in range(level_size):
+        #         node = queue.popleft()
+        #         level.append(node.val)
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #     result.append(level)
+        # return result
         if not root:
-            return []
-        result = []
+            return root
+        output = []
         queue = deque([root])
         while queue:
             level = []
@@ -20,5 +36,5 @@ class Solution:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            result.append(level)
-        return result
+            output.append(level)
+        return output
