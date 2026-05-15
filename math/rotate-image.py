e6336c607d810fbd907f758ef23b2d90b27452ee
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-05-15 18:44:03 -0400

    LC-rotate-image [Runtime: 0 ms; Memory: 19.3 MB]

--- math/rotate-image.py
+++ math/rotate-image.py
@@ -5,7 +5,7 @@ class Solution:
         """
         n = len(matrix)
         for i in range(n):
-            for j in range(i+1, n):
+            for j in range(i, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         for row in matrix:
             row.reverse()
