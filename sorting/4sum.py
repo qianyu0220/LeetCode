378c705a85006403e1447368a6f6b8d9889466f3
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-07-06 20:38:13 -0700

    LC-4sum [Runtime: N/A; Memory: N/A]

--- sorting/4sum.py
+++ sorting/4sum.py
@@ -1,28 +1,25 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
         n = len(nums)
-        nums.sort()
         output = []
+        nums.sort()
         for i in range(n-3):
             if i > 0 and nums[i] == nums[i-1]:
                 continue
             for j in range(i+1, n-2):
-                if j > i + 1 and nums[j] == nums[j-1]:
+                if j > i+1 and nums[j] == nums[j-1]:
                     continue
-                left = j + 1
-                right = n - 1
-                while left < right:
-                    total = nums[i] + nums[j] + nums[left] + nums[right]
+                x, y = j+1, n-1     
+                while x < y:
+                    total = nums[i] + nums[j] + nums[x] + nums[y]
                     if total == target:
-                        output.append([nums[i], nums[j], nums[left], nums[right]])
-                        left += 1
-                        right -= 1
-                        while left < right and nums[left] == nums[left - 1]:
-                            left += 1
-                        while left < right and nums[right+1] == nums[right]:
-                            right -= 1
+                        output.append([nums[i], nums[j], nums[x], nums[y]])
+                        x += 1
+                        while x < y and nums[x] == nums[x-1]:
+                            x += 1
                     elif total < target:
-                        left += 1
+                        x += 1
                     else:
-                        right -= 1
+                        y -= 1
         return output
+
