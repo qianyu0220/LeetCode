cf5a281936665501c15ea410a26b9da433ae6c04
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-07-07 22:20:44 -0700

    LC-search-insert-position [Runtime: N/A; Memory: N/A]

--- binary-search/search-insert-position.py
+++ binary-search/search-insert-position.py
@@ -1,14 +1,14 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
         n = len(nums)
-        left, right = 0, n-1
+        left = 0
+        right = n-1
         while left <= right:
             mid = (left + right) // 2
-            if nums[mid] == target:
+            if target == nums[mid]:
                 return mid
-            elif nums[mid] < target:
-                left = mid + 1
-            else:
+            elif target < nums[mid]:
                 right = mid - 1
-
+            else:
+                left = mid + 1
         return left
