0591659041fe5a790d3c272064bd7651498e44e7
qianyu0220 <123432067+qianyu0220@users.noreply.github.com>
2026-07-08 22:46:56 -0700

    LC-same-tree [Runtime: 0 ms; Memory: 19.4 MB]

--- breadth-first-search/same-tree.py
+++ breadth-first-search/same-tree.py
@@ -6,8 +6,13 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        if not p and not q:
+        # if not p and not q:
+        #     return True
+        # if not p or not q:
+        #     return False
+        # return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)       
+        if not p and not q: 
             return True
         if not p or not q:
             return False
-        return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)        
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right) and p.val == q.val
